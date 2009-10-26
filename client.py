ea7d2480c20983a48cceddbf9739a6d0f91ab18e
Niklas Pettersson <nikpe890@gmail.com>
2009-10-26 12:02:29 +0100

    En klient for meddelanden for N810

---
+++ client.py
@@ -0,0 +1,31 @@
+#client
+
+from socket import *
+import thread
+def receiver(clientSocket, ADDR):
+	i = 1
+	while 1:
+		data = clientSocket.recv(BUFF)
+		print data
+
+message = ""
+HOST = '130.236.189.14'
+PORT = 2010
+BUFF = 1024
+ADDR = (HOST, PORT)
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(ADDR)
+
+thread.start_new_thread(receiver, (clientSocket, ADDR))
+
+while 1:
+	data = raw_input()
+	message = data
+	if (data == '/quit'):
+		clientSocket.send('/quit')
+		break
+	clientSocket.send(data)
+	print "skickat:"+message
+
+clientSocket.close()
