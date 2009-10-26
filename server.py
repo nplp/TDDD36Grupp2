04209f0a2ad562d016fdd0b8db036eab0fc1cc59
KJ <kj@KJ.(none)>
2009-10-26 15:32:15 +0100

    ny server också

--- server.py
+++ server.py
@@ -7,14 +7,26 @@ import thread
 class socketClass:
 	active = 0
 
+
+class userClass:
+	name = "Player1"
+	password = "123"
+	def __init__(self,_name):
+		self.name = _name
+
+	def checkPass(self,_password):
+		if password == _password:
+			return true
+		return false
+
 def handler(index,zero):
 	try:
-		send("Server message: " + str(socketArray[index].ADDR) + " connected.")
+		sendAll("Server message: " + str(userArray[index].name) + " connected.")
 	
 		while 1:
 			data = socketArray[index].socket.recv(BUFF)
 			if(data == '/quit'): break
-			send (str(socketArray[index].ADDR) + " : " + data)
+			sendAll(str(userArray[index].name) + ": " + data)
 	except Exception, e:
 		print "client disconnected"
 	disconnect(index)
@@ -22,19 +34,20 @@ def handler(index,zero):
 	
 def disconnect(index):
 	socketArray[index].active = 0
-	send("Server message: " + " str(socketArray[index].ADDR)" + " disconnected.")
-	#Sedan ska det stå ett id istället för socket array bla bla, typ id[i] eller nått.
+	sendAll("Server message: " + str(userArray[index].name) + " disconnected.")
 	socketArray[index].socket.close()
 
-def send(message):	
+def sendAll(message):	
 	print message
 	for i in range(len(socketArray)):
 		if socketArray[i].active:
 			socketArray[i].socket.send(message)
-HOST = '130.236.189.14'
-PORT = 2011
+HOST = '130.236.219.209'
+PORT = 2026
 BUFF = 1024
 ADDR = (HOST, PORT)
+
+CLIENTNAME = "Player1"
 
 print "Binding serverSocket to: ", ADDR
 
@@ -43,14 +56,20 @@ serverSocket.bind(ADDR)
 serverSocket.listen(5)
 
 socketArray = list()
+userArray = list()
 
 print "Server meddelande: Servern aer redo."
 
 while 1:
 	socketArray.append(socketClass())
 	socketArray[len(socketArray) - 1].socket, socketArray[len(socketArray) - 1].ADDR = copy(serverSocket.accept())
-	socketArray[len(socketArray) - 1].active = 1;
 	
+	socketArray[len(socketArray) - 1].socket.send("Type a name: ")
+	CLIENTNAME = socketArray[len(socketArray) - 1].socket.recv(BUFF)
+
+	userArray.append(userClass(CLIENTNAME))
+	socketArray[len(socketArray) - 1].active = 1;
+
 	thread.start_new_thread(handler, ((len(socketArray) - 1),0))
 
 serverSocket.close()
