523efe9a65d64fecaae71079aeacadf19fec4cb0
unknown <Owe@.(none)>
2009-10-28 10:55:33 +0100

    testar en ny cpu funktion

--- CPU.py
+++ CPU.py
@@ -10,7 +10,7 @@ hal_obj = bus.get_object ('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
 hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 
 # find all devices that have the capability 'laptop_panel'
-udis = hal.FindDeviceByCapability ('cpu')
+udis = hal.FindDeviceByCapability ('processor')
 
 dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
 print dev_obj.GetCPUFreqGovernor()
