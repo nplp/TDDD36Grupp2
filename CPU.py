767619bd5ba31c0869c7fc66913dcf0d115a74e8
unknown <Owe@.(none)>
2009-10-28 13:42:37 +0100

    lite lek bara

--- CPU.py
+++ CPU.py
@@ -10,7 +10,7 @@ hal_obj = bus.get_object ('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
 hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 
 # find all devices that have the capability 'laptop_panel'
-udis = hal.FindDeviceByCapability ('laptop_panel')
+uids = hal.FindDeviceByCapability ('laptop_panel')
 
 
 
