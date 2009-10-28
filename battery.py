7829b4eb87d33c181b8856ab43aae6f05cb778f3
unknown <Owe@.(none)>
2009-10-28 09:01:09 +0100

    testar en ny batteri funktion

--- battery.py
+++ battery.py
@@ -7,12 +7,14 @@ hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 uids = hal.FindDeviceByCapability('battery')
 dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
 
+print 'charge level percentage',\
+      dev_obj.GetProperty('battery.charge_level.percentage')
 #print 'charge design', dev_obj.GetProperty('battery.reporting.design')
 #print 'charge current', dev_obj.GetProperty('battery.reporting.current')
 x = float(dev_obj.GetProperty('battery.reporting.current'))
 y = float(dev_obj.GetProperty('battery.reporting.design'))
 
-print 'battery level: ', x/y
+print 'battery level: ', (x/y)*100,'%'
 #print 'charge unit', dev_obj.GetProperty('battery.reporting.unit')
 
 #print 'voltage current', dev_obj.GetProperty('battery.voltage.current')
