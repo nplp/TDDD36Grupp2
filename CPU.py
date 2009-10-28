8fe24950dd1b4d333bb6788fb4bdd002bbfdd42d
unknown <Owe@.(none)>
2009-10-28 11:17:39 +0100

    testar en ny cpu funktion

--- CPU.py
+++ CPU.py
@@ -9,12 +9,23 @@ bus = dbus.SystemBus ()
 hal_obj = bus.get_object ('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
 hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 
-#devices = hal.GetAllDevices()
-#for i in devices:
- #       print i
-
 # find all devices that have the capability 'laptop_panel'
-uids = hal.FindDeviceByCapability ('computer')
+udis = hal.FindDeviceByCapability ('laptop_panel')
 
-dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
-print dev_obj.GetCPUFreqGovernor()
+for udi in udis:
+        print udi
+
+        # get a device object
+        dev_obj = bus.get_object ('org.freedesktop.Hal', udi)
+
+        # get an interface to the device
+        dev = dbus.Interface (dev_obj, 'org.freedesktop.Hal.Device')
+        print dev.GetProperty ('info.product')
+        print "Brightness levels:", dev.GetProperty ('laptop_panel.num_levels')
+
+        # get a difference interface to the device
+        dev = dbus.Interface (dev_obj, 'org.freedesktop.Hal.Device.LaptopPanel')
+        # make some function calls
+        print "Current brightness:", dev.GetBrightness ()
+        dev.SetBrightness (int (sys.argv[1]))
+        print "New brightness:", dev.GetBrightness ()
