d17aac4beccdb6e81371afdc4f20b6d3a225d831
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-02 10:26:49 +0100

    A rockin client

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-#client
+# client
 # coding:utf-8
-# Ovanst￥ende rad ￤r ISO-kodning f￶r att ￥￤￶ ska funka.
+# Ovanst￯﾿ﾥende rad ￯﾿ﾤr ISO-kodning f￯﾿ﾶr att ￯﾿ﾥ￯﾿ﾤ￯﾿ﾶ ska funka.
 
 import re
 import sys
@@ -9,7 +9,7 @@ from threading import *
 import os
 from message import *
 from time import time
-import dbus
+#import dbus
 
 #checkar servern
 def checkServer():
@@ -19,66 +19,68 @@ def checkServer():
         serverSocket.connect((HOST, PORT))
     except error:
         return 1
+    
+def connect():
+    status = checkServer()
+    if (status):
+        print "poop"
+        ADDR = (HOST2, PORT)
+    else:
+        print "score"
+        ADDR = (HOST, PORT)
+    
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect(ADDR)
+
+        recThread = recieverClass(clientSocket, ADDR)
+        recThread.start()
+
 def checkBattery():
-	try:
-		bus = dbus.SystemBus()
-		hal_obj = bus.get_object ('org.freedesktop.Hal', 
-               	           '/org/freedesktop/Hal/Manager')
-		hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
-		uids = hal.FindDeviceByCapability('battery')
-		dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
-		x = float(dev_obj.GetProperty('battery.reporting.current'))
-		y = float(dev_obj.GetProperty('battery.reporting.design'))
-		bat = int((x/y)*100)
-		if(bat < 20):
-			print 'Nu har du',bat,'% kvar i batteri.'
-	except Exception, e :
-			print "Du sitter pa en loser dator och har inget batteri"	
+    try:
+        bus = dbus.SystemBus()
+        hal_obj = bus.get_object ('org.freedesktop.Hal', 
+                              '/org/freedesktop/Hal/Manager')
+        hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
+        uids = hal.FindDeviceByCapability('battery')
+        dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
+        x = float(dev_obj.GetProperty('battery.reporting.current'))
+        y = float(dev_obj.GetProperty('battery.reporting.design'))
+        bat = int((x/y)*100)
+        if(bat < 20):
+            print 'Nu har du',bat,'% kvar i batteri.'
+    except Exception, e :
+            print "Du sitter pa en loser dator och har inget batteri"       
+
 class recieverClass(Thread):
-	def __init__(self, _clientSocket, _ADDR):
-		self.clientSocket = _clientSocket
-		self.ADDR = _ADDR
-		Thread.__init__(self)
-	
-	# Tar emot meddelanden
-	def reciever(self):
-		checkBattery()
-		try:
-			while 1:
-				data = unicode(self.clientSocket.recv(BUFF), 'utf-8')
-				if(data != "" and data != " "):
-					if(data.startswith('/ping')):
-						s = data.split(' ', 1)
-						print s[0] + " " + str(time() - float(s[1]))
-					else:
-						print data
-		except Exception, e:
-			print "Connection lost"
+    def __init__(self, _clientSocket, _ADDR):
+        self.clientSocket = _clientSocket
+        self.ADDR = _ADDR
+        Thread.__init__(self)
+    
+    # Tar emot meddelanden
+    def reciever(self):
+        try:
+            while 1:
+                data = unicode(self.clientSocket.recv(BUFF), 'utf-8')
+                if(data != ""):
+                    if(data.startswith('/ping')):
+                        s = data.split(' ', 1)
+                        print s[0] + " " + str(time() - float(s[1]))
+                    else:
+                        print data
+        except Exception, e:
+            print "Connection lost"
 
-	def run(self):
-		self.reciever()
+    def run(self):
+        self.reciever()
 
 
-HOST = '130.236.189.14'
+HOST = '130.236.189.22'
 HOST2 = '130.236.216.90'
-PORT = 2020
+PORT = 2066
 BUFF = 1024
 
-status = checkServer()
-if (status):
-    print "poop"
-    ADDR = (HOST2, PORT)
-else:
-    print "score"
-    ADDR = (HOST, PORT)
-    
-print ADDR
-
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect(ADDR)
-
-recThread = recieverClass(clientSocket, ADDR)
-recThread.start()
+connect()
 
 # Skickar meddelanden samt har hand om kommandon
 while 1:
@@ -91,7 +93,12 @@ while 1:
         break
     if(data.startswith('/ping')):
         data = '/ping' + " " + str(time())
-        
-    clientSocket.send(data)
+    try:    
+        clientSocket.send(data)
+    except Exception, e:
+        print "Server has gone down, redirecting."
+        print "The last messeage you sent had been lost and will have to resent"
+        connect()
+    
 
 clientSocket.close()
