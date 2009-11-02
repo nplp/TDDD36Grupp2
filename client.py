bfa2eadc677b97ef83e470134b79cba6d49fb77d
Niklas Pettersson <nikpe890@gmail.com>
2009-11-02 09:11:38 +0100

    testar igen

--- client.py
+++ client.py
@@ -28,7 +28,7 @@ def checkBattery():
 	dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
 	x = float(dev_obj.GetProperty('battery.reporting.current'))
 	y = float(dev_obj.GetProperty('battery.reporting.design'))
-	if(int((x/y)*100) < 60))
+	if(int((x/y)*100) < 60):
 	print 'Nu har du',int(x/y),'% kvar i batteri.'
 
 class recieverClass(Thread):
@@ -58,7 +58,7 @@ class recieverClass(Thread):
 
 HOST = '130.236.189.14'
 HOST2 = '130.236.216.90'
-PORT = 2046
+PORT = 2040
 BUFF = 1024
 
 status = checkServer()
