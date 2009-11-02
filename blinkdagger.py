d47a72573c3d3d14edb816e58ff950185dfcdad7
unknown <Owe@.(none)>
2009-11-02 16:04:09 +0100

    k

--- blinkdagger.py
+++ blinkdagger.py
@@ -22,13 +22,28 @@ def blink_cb(device, loop):
 
     return True
 
+def blink_cb2(device, loop):
+    
+    global count
+
+    device.display_state_off()
+
+    count += 1 
+    if count == 5:
+        loop.quit()
+        return False
+
+    return True
+
 def main():
     global count
     loop = gobject.MainLoop()
     osso_c = osso.Context("osso_test_device_on", "0.0.1", False)
     device = osso.DeviceState(osso_c)
     
-    gobject.timeout_add(11000, blink_cb, device, loop)
+    gobject.timeout_add(6000, blink_cb, device, loop)
+    gobject.timeout_add(6000, blink_cb2, device, loop)
+
 
     loop.run()
 
