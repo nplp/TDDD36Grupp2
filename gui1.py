5d186dc96723b5e26eabd0869ccb484b54b645c1
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-04 11:43:29 +0100

    gui

--- gui1.py
+++ gui1.py
@@ -1,62 +1,53 @@
-# Set size request
-
+#Ar det inte battre om detta sparas i en databas som man kan anvanda sig av?
+#Jag kan inte se min progressbar, vad hander egentligen?
 #!/usr/bin/env python
-# example table.py
 # -*- coding: utf-8 -*-
 import pygtk
 pygtk.require('2.0')
+import gtk, gobject
 import gtk
 import sys
-#sys.path.append('./kartkomponent')
+sys.path.append('./kartkomponent/kart')
 import data_storage
 import map_xml_reader
 import gui_map
 import gui
 
+def progress_timeout(self, pbobj):
+    	if pbobj.activity_check.get_active():
+		pbobj.pbar.pulse()
+	else:
+        	new_val = pbobj.pbar.get_fraction() + 0.01
+        	if new_val > 1.0:
+			new_val = 0.0
+        # Set the new value
+       	pbobj.pbar.set_fraction(new_val)
+	return True
+
 class MenuExample:
+
+    def toggle_activity_mode(self, widget, data=None):
+        if widget.get_active():
+            self.pbar.pulse()
+        else:
+            self.pbar.set_fraction(0.0)
+
+    def toggle_orientation(self, widget, data=None):
+        if self.pbar.get_orientation() == gtk.PROGRESS_LEFT_TO_RIGHT:
+            self.pbar.set_orientation(gtk.PROGRESS_RIGHT_TO_LEFT)
+        elif self.pbar.get_orientation() == gtk.PROGRESS_RIGHT_TO_LEFT:
+            self.pbar.set_orientation(gtk.PROGRESS_LEFT_TO_RIGHT)
+	    
+    
 	
     def callback(self, widget, data=None):
         print "Hello again - %s was pressed" % data
 	
-	#Vilka knappar ska visas?
-	#Kommunikation
-    def komm(self, widget, event, data=None):
-	self.text.show()
-	self.samtal.show()
-	self.video.show()
-	self.uppdragsmall.hide()
-	self.karta.hide()
-	self.uppdrag.hide()
-	self.rapport.hide()
-	self.lager.hide()
-	self.tillbaka.show()
-		
-	#Vektyg	
-    def verk(self, widget, event, data=None):
-	self.text.hide()
-	self.samtal.hide()
-	self.video.hide()
-	self.uppdragsmall.show()
-	self.karta.show()
-	self.uppdrag.hide()
-	self.rapport.hide()
-	self.lager.hide()
-	self.tillbaka.show()
-	    
-	#Filer
-    def fil(self,widget,event,data=None):
-	 self.text.hide()
-	 self.samtal.hide()
-	 self.video.hide()
-	 self.uppdragsmall.hide()
-	 self.karta.hide()
-	 self.uppdrag.show()
-	 self.rapport.show()
-	 self.lager.show()
-	 self.tillbaka.show()
-	    
 	#Tillbaka
     def tbaka(self,widget,event,data=None):
+	 self.verktyg.set_active(False)
+	 self.filer.set_active(False)
+	 self.kommunikation.set_active(False)
 	 self.text.hide()
 	 self.samtal.hide()
 	 self.video.hide()
@@ -67,10 +58,87 @@ class MenuExample:
 	 self.lager.hide()	
 	 self.tillbaka.hide()
 	
+	#Rapport
+    def rapp(self,widget,event,data=None):
+	 self.filer.set_active(False)
+	 self.text.hide()
+	 self.samtal.hide()
+	 self.video.hide()
+	 self.uppdragsmall.hide()
+	 self.karta.hide()
+	 self.uppdrag.hide()
+	 self.rapport.hide()
+	 self.lager.hide()	
+	 self.tillbaka.hide()
+	 self.startakarta.hide()
+	 self.label.hide()
+	 self.vbox4.show()
+	
+	#Kommunikation
+    def komm(self, widget, event, data=None):
+	if widget.get_active():
+		self.verktyg.set_active(False)
+		self.filer.set_active(False)
+          	self.text.show()
+		self.samtal.show()
+		self.video.show()
+		self.uppdragsmall.hide()
+		self.karta.hide()
+		self.uppdrag.hide()
+		self.rapport.hide()
+		self.lager.hide()
+		self.tillbaka.show()
+      	else:
+          	self.tbaka(widget, data)		
+		
+	#Vektyg	
+    def verk(self, widget, event, data=None):
+	if widget.get_active():
+		self.kommunikation.set_active(False)
+		self.filer.set_active(False)
+		self.text.hide()
+		self.samtal.hide()
+		self.video.hide()
+		self.uppdragsmall.show()
+		self.karta.show()
+		self.uppdrag.hide()
+		self.rapport.hide()
+		self.lager.hide()
+		self.tillbaka.show()
+	else:
+          	self.tbaka(widget, data)
+	    
+	#Filer
+    def fil(self,widget,event,data=None):
+	 if widget.get_active():
+		self.kommunikation.set_active(False)
+		self.verktyg.set_active(False)
+		self.text.hide()
+		self.samtal.hide()
+		self.video.hide()
+		self.uppdragsmall.hide()
+		self.karta.hide()
+		self.uppdrag.show()
+		self.rapport.show()
+		self.lager.show()
+		self.tillbaka.show()
+	 else:
+          	self.tbaka(widget, data)
+	
 	#Visa kartan
     def kartan(self, widget, event, data=None):
-	self.startakarta.show()
-		
+	 self.verktyg.set_active(False)
+	 self.text.hide()
+	 self.samtal.hide()
+	 self.video.hide()
+	 self.uppdragsmall.hide()
+	 self.karta.hide()
+	 self.uppdrag.hide()
+	 self.rapport.hide()
+	 self.lager.hide()	
+	 self.tillbaka.hide()
+	 self.label.hide()
+	 self.startakarta.show()
 	
         #Avsluta programmet
     def delete_event(self, widget, event, data=None):
@@ -84,35 +152,30 @@ class MenuExample:
         self.window.set_title("GUI")
         self.window.connect("delete_event", lambda w,e: gtk.main_quit())
 	
-	
-	
 	#Skapa en stor Hbox
 	self.hbox = gtk.HBox(False, 0)
 	
-
-
-	
-	#Skapa en Vbox som sedan ska in i Hboxen och innehallar permanenta knapparna i menyn
+	#Skapa en Vbox som sedan ska in i Hboxen och innehallar huvudknapparna i menyn
         self.vbox = gtk.VBox(False, 0)
 	self.vbox.set_size_request(198, 95)
         self.vbox.show()
 
 	#Knappar stor meny
         # Kommunikation
-        self.kommunikation = gtk.Button("Kommunikation")
-        self.kommunikation.connect("clicked", self.komm, "Kommunikation")
+        self.kommunikation = gtk.ToggleButton("Kommunikation")
+        self.kommunikation.connect("toggled", self.komm, "Kommunikation")
         self.kommunikation.show()
         self.vbox.pack_start(self.kommunikation, True, True, 0)
 
         # Verktyg
-        self.verktyg = gtk.Button("Verktyg")
-        self.verktyg.connect("clicked", self.verk, "Verktyg")
+        self.verktyg = gtk.ToggleButton("Verktyg")
+        self.verktyg.connect("toggled", self.verk, "Verktyg")
         self.verktyg.show()
 	self.vbox.pack_start(self.verktyg, True, True, 0)
 
     	# Filer
-        self.filer= gtk.Button("Filer")
-        self.filer.connect("clicked", self.fil, "Filer")
+        self.filer= gtk.ToggleButton("Filer")
+        self.filer.connect("toggled", self.fil, "Filer")
         self.vbox.pack_start(self.filer, True, True, 0)
 	self.filer.show()
 
@@ -122,15 +185,26 @@ class MenuExample:
 	self.vbox.pack_start(self.avsluta, True, True,0)
         self.avsluta.show()
 	
+	#Progressbar:
+        self.pbar = gtk.ProgressBar()
+	self.timer = gobject.timeout_add(100, self.progress_timeout, self)
+	self.vbox.pack_start(self.pbar, True, True,0)
+	self.pbar.show()
+	
+	#right to left
+	# Add a check button to toggle orientation
+        check = gtk.CheckButton("Right to Left")
+        check.connect("clicked", self.toggle_orientation)
+        check.show()
+	self.vbox.pack_start(check, True, True,0)
+
 	self.hbox.pack_start(self.vbox, False, False, 0)
 	
-	
-
 	#Knappar i undermenyer
 	#Knappar under kommunikation
 	#i en vbox
         self.vbox2 = gtk.VBox(True, 0)
-	#self.vbox2.set_size_request(198, 95)
+	self.vbox2.set_size_request(198, 95)
         self.vbox2.show()
 	
 	# Textmeddelande
@@ -159,7 +233,6 @@ class MenuExample:
 	self.uppdragsmall.connect("clicked", self.callback, "Uppdragsmall")
 	self.vbox2.pack_start(self.uppdragsmall, True, True,0)
 	
-	
 	#Karta
 	self.karta = gtk.Button("Karta")
 	self.karta.connect("clicked", self.kartan, "Karta")
@@ -178,7 +251,7 @@ class MenuExample:
 	
 	#Rapport
 	self.rapport = gtk.Button("Rapport")
-	self.rapport.connect("clicked", self.callback, "Rapport")
+	self.rapport.connect("clicked", self.rapp, "Rapport")
 	self.vbox2.pack_start(self.rapport, True, True,0)
 	
 	#Lager
@@ -216,20 +289,19 @@ class MenuExample:
 						"arc(x - 5, y - 5, 10, 0, 2 * math.pi)",
 						"set_source_rgb(0, 0, 0)"))
 	self.startakarta = gui_map.Map(map)
-	
 	self.vbox3 = gtk.VBox(False, 0)
         self.vbox3.show()
 	
 	#create a new label.
-        label = gtk.Label("Anvandare	symbol	datum tid")
-        label.set_alignment(0, 0)
-        self.vbox3.pack_start(label, False, False, 0)
-        label.show()
+        self.label = gtk.Label("Anvandare	symbol	datum tid")
+        self.label.set_alignment(0, 0)
+        self.vbox3.pack_start(self.label, False, False, 0)
+        self.label.show()
 	
+	#Packa karta
 	self.vbox3.pack_start(self.startakarta,True,True,0)
-	
-	#self.startakarta.show()
 	self.hbox.pack_start(self.vbox3, True, True, 0)
+	#self.hbox.pack_start(self.vbox4, True, True, 0)
     	self.hbox.show()
 	self.window.add(self.hbox)
         self.window.show()
