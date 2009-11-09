b7476dcfc37cc84fbf72af779159cc980c8f553c
KJ <kj@KJ.(none)>
2009-11-09 14:57:13 +0100

    Databasen och servern snackar.

---
+++ message.py
@@ -0,0 +1,29 @@
+# Message
+# coding:utf-8
+# Ovanstående rad är ISO-kodning för att åäö ska funka.
+
+cmdArray = ("/cleanup", "/exit", "/list", "/ping", "/quit", "/reply","/status", "/whisper")
+
+class Message:
+	text = ""
+	def __init__(self, _text):
+		self.text = _text
+
+	def isCommand(self):
+		if(len(self.text)>0):
+			if(self.text[0] == '/'):
+				return 1
+		return 0
+
+def finishCMD(msg):
+	if(len(msg.text)>1):
+		cat = str.split(msg.text,' ',1)
+		
+		for cmd in cmdArray:
+			if cmd.startswith(cat[0]):
+				if(len(cat)>1):
+					return cmd + " " + cat[1]
+				else:	return cmd
+	return msg.text
+
+
