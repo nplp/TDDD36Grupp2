c3e209fd32db731591869b88f89001256d7d0484
unknown <Owe@.(none)>
2009-11-09 14:08:52 +0100

    tja

--- osso_receiver.py
+++ osso_receiver.py
@@ -4,6 +4,7 @@ import gtk
  
 def callback_func(interface, method, arguments, user_data):
     osso_c = user_data
+    print "hejsan"
  
 osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
 rpc = osso.Rpc(osso_c)
