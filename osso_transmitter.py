4b0afe59b71139a5c90db49fe5732ee9690dfc26
unknown <Owe@.(none)>
2009-11-10 14:05:21 +0100

    push push lady lightning

--- osso_transmitter.py
+++ osso_transmitter.py
@@ -5,10 +5,11 @@ import gtk
  
 def send_rpc(widget, osso_c):
     rpc = osso.Rpc(osso_c)
+    x="hejsan detta aer en jattelang strang for att testa funktionen"
     rpc.rpc_run("spam.eggs.osso_test_receiver",
                    "/spam/eggs/osso_test_receiver",
                    "spam.eggs.osso_test_receiver",
-                   "hej_kj")
+                   x)
  
 osso_c = osso.Context("osso_test_sender", "0.0.1", False)
  
