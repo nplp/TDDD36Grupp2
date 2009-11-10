0db730cf292c606297394f06c541be52beb13076
Niklas Pettersson <nikpe890@gmail.com>
2009-11-10 16:45:21 +0100

    testar en gps applikation

--- gpsapp.py
+++ gpsapp.py
@@ -8,12 +8,12 @@ oldcoordx,oldcoordy = (0,0)
 i = 0
 def waiting_for_a_fix():
 	print "Vi vantar pa en koordinat"
-	while ((coordx,coordy) == ((0,0) || (oldcoordx,oldcoordy))):
-		(coordx,coordy) =gps.get_position()
+	while ((coordx == 0 and coordy == 0) or (coordx == oldcoordx and coordy == oldcoordy)):
+		coordx,coordy =gps.get_position()
     		print "Wai-ting."+i
 		i+=1
     		time.sleep(2)
-	(oldcoordx,oldcoordy) = gps.get_position()
+	oldcoordx,oldcoordy = gps.get_position()
 	print gps.get_position()
 
 # Startar GPSEN
