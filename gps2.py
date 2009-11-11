f10ee9626d31603aca051bf16b642230c88cd6e7
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-11 11:54:53 +0100

    my gps

---
+++ gps2.py
@@ -0,0 +1,29 @@
+ import gpsbt
+ import time
+ 
+ def main():
+     context = gpsbt.start()
+ 
+     if context == None:
+         print 'Problem while connecting!'
+         return
+ 
+     # ensure that GPS device is ready to connect and to receive commands
+     time.sleep(2)
+     gpsdevice = gpsbt.gps()
+ 
+     # read 3 times and show information
+     for a in range(4):
+         gpsdevice.get_fix()
+         time.sleep(2)
+ 
+         # print information stored under 'fix' variable
+         print 'Altitude: %.3f'%gpsdevice.fix.altitude
+         # dump all information available
+         print gpsdevice
+ 
+     # ends Bluetooth connection
+     gpsbt.stop(context)
+ 
+ main()
+
