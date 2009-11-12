09669357ac7bcb453528efc83dacafcf079a6bfa
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-12 11:46:54 +0100

    boobies

--- kartkomponent/main.py
+++ kartkomponent/main.py
@@ -91,6 +91,28 @@ def updatemap():
 
 		map.gui_draw()
 
+
+# Kartan
+print "Läser in kartinformation från kartdata/map.xml"
+mapxml = map_xml_reader.MapXML("kartdata/map.xml")
+map = data_storage.MapData(mapxml.get_name(),
+mapxml.get_levels())
+
+# Ställer in vad kartkomponenten ska fokusera på (visa)
+# (blir mittenpunkten på skärmen, dvs 50% x-led, 50% y-lyd.
+map.set_focus(longitude, latitude)
+# Ritar ut tre objekt
+map.add_object("Ambulans1", data_storage.MapObject({"longitude":15.57796,
+		                                    "latitude":58.40479},
+		                                   "ikoner/ambulans.png"))
+map.add_object("Brandbil1", data_storage.MapObject({"longitude":15.5729,
+		                                    "latitude":58.40193},
+		                                   "ikoner/brandbil.png"))
+map.add_object("Sjukhus1", data_storage.MapObject({"longitude":15.5629,
+		                                   "latitude":58.4093},
+		                                  "ikoner/sjukhus.png"))
+
+
 # Skapar grafiska interfacet.
 print "Skapar programmets GUI."
 app = gui.Gui(map)
