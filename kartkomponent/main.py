48300729851254f6836e18b0ae93c0bcb8b8d7be
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-12 14:54:31 +0100

    csn manager

--- kartkomponent/main.py
+++ kartkomponent/main.py
@@ -7,7 +7,7 @@ import gpsbt
 import thread
 
 latitude,longitude = (58.4120,15.5762)
-hej = True
+hej == True
 
 # Spec enbart för testing
 def trixy():
@@ -101,8 +101,7 @@ waiting_for_a_fix()
 # Kartan
 print "Läser in kartinformation från kartdata/map.xml"
 mapxml = map_xml_reader.MapXML("kartdata/map.xml")
-map = data_storage.MapData(mapxml.get_name(),
-mapxml.get_levels())
+map = data_storage.MapData(mapxml.get_name(),mapxml.get_levels())
 
 # Ställer in vad kartkomponenten ska fokusera på (visa)
 # (blir mittenpunkten på skärmen, dvs 50% x-led, 50% y-lyd.
