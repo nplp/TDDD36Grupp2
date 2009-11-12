063d4b2b2302df52287b99b155129ee8106e898a
Niklas Pettersson <nikpe890@gmail.com>
2009-11-12 13:48:00 +0100

    readd till mathias

--- readdb.py
+++ readdb.py
@@ -4,9 +4,13 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 import os
+from datetime import *
 engine = create_engine('sqlite:///data.db', echo=False)
 metadata = MetaData()
-
+#genererar ett unikt id åt varje objekt (mission, alarm, poi, unit)
+def generate_id():
+	id+=1
+	return id
 
 group_table = Table('group', metadata,
 	Column('id', Integer, primary_key=True),
@@ -31,13 +35,48 @@ user_group = Table('user_group', metadata,
 	Column('user_id', None, ForeignKey('users.id'), primary_key=True),
 	Column('group_id', None, ForeignKey('group.id'), primary_key=True)
 	)
+	
 mission_table =Table('missions', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('title', String(30)),
-	Column('body', String(500)),
-	Column('time', String(40)),
-	Column('location', String(50))
+	Column('poi_id', Integer),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('contact_person', String(50)),
+	Column('contact_number', String(50)),
+	Column('type', String(50)),
+	Column('subtype', String(50)),
+	Column('description', Text()),
+	Column('status', String(50)),
+	Column('finishtime', String(50))
 	)
+poi_table =Table('pois', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('coordx', Float),
+	Column('coordy', Float),
+	Column('name', Float),
+	Column('timestamp', Float),
+	Column('type', String(50)),
+	Column('subtype', String(50))
+	)
+alarm_table =Table('alarms', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('type', String(50)),
+	Column('subtype', String(50)),
+	Column('contact_number', String(50)),
+	Column('contact_person', String(50)),
+	Column('extra_info', String(200)),
+	)
+unit_table = Table('units',metadata,
+	Column('id', Integer, primary_key=True),
+	Column('coordx', Float),
+	Column('coordy', Float),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('type', String(50))
+	)
+	
 mission_group = Table('mission_group', metadata,
 	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
 	Column('group_id', None, ForeignKey('group.id'), primary_key=True)
@@ -66,17 +105,64 @@ class Group(object):
 		self.name=name
 	def __repr__(self):
 		return self.name
-class Mission(object):
-	def __init__(self, title=None, body=None, time=None, location=None):
-		self.title=title
-		self.body=body
-		self.time=time
-		self.location=location
+#class Mission(object):
+	#def __init__(self, title=None, body=None, time=None, location=None):
+		#self.title=title
+		#self.body=body
+		#self.time=time
+		#self.location=location
+class mission(object):
+	def __init__(self, poi_id = None, id=None, name= None, timestamp=None, type= None, sub_type= None, description = None, contact_person = None, contact_number = None, status = None, finishtime = None):
+		#self.id = generate_id()
+		self.poi_id = poi_id
+		self.id=id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.contact_person = contact_person
+		self.contact_number = contact_number
+		self.sub_type = sub_type
+		self.description = description
+		self.status = status
+		self.finishtime = finishtime
+class poi(object):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type=None, sub_type= None):
+		self.coordx = coordx
+		self.coordy = coordy
+		self.id = id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.sub_type = sub_type
 	
+		
+class alarm(object):
+	def __init__(self, id=None, name= None, timestamp=None, type= None, poi_id=None, contact_person= None, contact_number= None, extrainfo = None):
+		self.id=id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.poi_id=poi_id
+		self.contact_person = contact_person
+		self.contact_number = contact_number
+		self.extrainfo = extrainfo
+
+class unit(object):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type= None):
+		self.coordx = coordx
+		self.coordy = coordy
+		self.id = id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		
 metadata.create_all(engine)
 mapper(Group, group_table)
+mapper(poi, poi_table)
+mapper(alarm, alarm_table)
+mapper(unit, unit_table)
 
-mapper(Mission, mission_table, properties=dict(
+mapper(mission, mission_table, properties=dict(
 	groups=relation(Group, secondary= mission_group, backref='missions'))
 	)
 mapper(User, user_table, properties=dict(
@@ -104,7 +190,7 @@ def getTotal(namn):
 		temp = item.count + temp
 	return temp
 #user2.groups.append(group2)
-m= Mission()
+m= mission()
 m.title = 'Bombdad'
 m.body= 'en cyckel parkering ar sprangd bygg en ny cyckel parkering'
 m.time='kl 15:00 den 11 november 2009'
@@ -112,7 +198,7 @@ m.location ='Linkoping'
 
 g=get_group('team2')
 
-print get_group_users('team2')
+#print get_group_users('team2')
 m.groups.append(g)
 
 #print get_group_users(m.groups[0])
