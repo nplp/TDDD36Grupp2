e66b129f2f5dcc3d96ddadfb032ecf0c896d7edb
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-12 16:14:58 +0100

    csn manager

--- kartkomponent/main.py
+++ kartkomponent/main.py
@@ -35,50 +35,21 @@ def waiting_for_a_fix():
 # En loop som uppdaterar kartan med jämna mellanrum
 def updatemap():
 	while(hej == True):
-		# Kartan
-		print "Läser in kartinformation från kartdata/map.xml"
-		mapxml = map_xml_reader.MapXML("kartdata/map.xml")
-
-		map = data_storage.MapData(mapxml.get_name(),
-				           mapxml.get_levels())
-
-		# Ställer in vad kartkomponenten ska fokusera på (visa)
-		# (blir mittenpunkten på skärmen, dvs 50% x-led, 50% y-lyd.
-		map.set_focus(longitude, latitude)
-
-		# Ritar ut tre objekt
-		map.add_object("Ambulans1", data_storage.MapObject({"longitude":15.57796,
-				                                    "latitude":58.40479},
-				                                   "ikoner/ambulans.png"))
-		map.add_object("Brandbil1", data_storage.MapObject({"longitude":15.5729,
-				                                    "latitude":58.40193},
-				                                   "ikoner/brandbil.png"))
-		map.add_object("Sjukhus1", data_storage.MapObject({"longitude":15.5629,
-				                                   "latitude":58.4093},
-				                                  "ikoner/sjukhus.png"))
-
-		# Andra exempel på kommandon finns här:
-		#   http://www.tortall.net/mu/wiki/CairoTutorial
-		#   http://www.tortall.net/mu/wiki/PyGTKCairoTutorial
-
-		map.add_object("Shape1", data_storage.MapObject({"longitude":15.5829,
-				                                 "latitude":58.4093},
-				                                "arc(x - 5, y - 5, 10, 0, 2 * math.pi)",
-				                                "set_source_rgb(0, 0, 0)"))
-
+		print "lägg till objekt"		
 		map.add_object("Shape2", data_storage.MapObject({"longitude":longitude,
 				                             "latitude":latitude},
 				                             "arc(x - 6, y - 6, 12, 0, 2 * math.pi)",
 				                             "set_source_rgb(0, 0, 0)"))	
-	
 		print "Sov din javel"
 		time.sleep(10.0)
+		
+		print "redraw"
+		map.queue_draw()
 
 		# Uppdaterar dina kordinater
 		print "going to trixy"
 		trixy()
 
-		map.queue_draw()
 
 
 
@@ -124,8 +95,8 @@ app = gui.Gui(map)
 
 # Kör programmet
 print "Kör programmet."
-app.run()
-
-thread.start_new_thread(updatemap, ())
+thread.start_new_thread(app.run, ())
+print "Innan updatemap"
+updatemap()
 
 
