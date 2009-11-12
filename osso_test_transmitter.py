b832bbce32d309e806cf97343180f7f007a29120
unknown <Owe@.(none)>
2009-11-12 11:00:40 +0100

    push push lady lightning

---
+++ osso_test_transmitter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2.5
+import osso
+import hildon
+import gtk
+ 
+def send_rpc(widget, osso_c):
+    rpc = osso.Rpc(osso_c)
+    rpc.rpc_run("spam.eggs.osso_test_receiver",
+                   "/spam/eggs/osso_test_receiver",
+                   "spam.eggs.osso_test_receiver", "hej_kj")
+ 
+osso_c = osso.Context("osso_test_sender", "0.0.1", False)
+ 
+window = hildon.Window()
+window.connect("destroy", gtk.main_quit)
+send_button = gtk.Button("Send RPC")
+hbox = gtk.HBox(homogeneous=False, spacing=0)
+window.add(hbox)
+hbox.add(send_button)
+send_button.connect("clicked",
+                    send_rpc, osso_c)
+window.show_all()
+
+gtk.main() 
