3cd2584e599f0f3037e38275cd6cde0279a38f50
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-13 10:45:32 +0100

    server databas

--- server.py
+++ server.py
@@ -29,30 +29,86 @@ from sqlalchemy.orm import *
 engine = create_engine('sqlite:///data.db', echo=False)
 metadata = MetaData()
 
-users_table = Table('users_table', metadata,
+group_table = Table('group', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(40)),
-	Column('clearance', String(40)),
-	Column('group', String(20)),
-	Column('password', String(20))
+	Column('name', String(20))
 	)
-items_table = Table('items_table', metadata,
+	
+user_table = Table('users', metadata,
 	Column('id', Integer, primary_key=True),
+	Column('name', String(20)),
+	Column('clearance', String(40)),
+	Column('password', String(40))
+	)
+	
+items_table = Table('items_table', metadata,
+	Column('item_id', Integer, primary_key=True),
 	Column('name', String(40)),
 	Column('count', Integer),
 	Column('location', String(40))
 	)
+
+user_group = Table('user_group', metadata,
+	Column('user_id', None, ForeignKey('users.id'), primary_key=True),
+	Column('group_id', None, ForeignKey('group.id'), primary_key=True)
+	)
+	
+mission_table =Table('missions', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('poi_id', Integer),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('contact_person', String(50)),
+	Column('contact_number', String(50)),
+	Column('type', String(50)),
+	Column('subtype', String(50)),
+	Column('description', Text()),
+	Column('status', String(50)),
+	Column('finishtime', String(50))
+	)
+poi_table =Table('pois', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('coordx', Float),
+	Column('coordy', Float),
+	Column('name', Float),
+	Column('timestamp', Float),
+	Column('type', String(50)),
+	Column('subtype', String(50))
+	)
+alarm_table =Table('alarms', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('type', String(50)),
+	Column('subtype', String(50)),
+	Column('contact_number', String(50)),
+	Column('contact_person', String(50)),
+	Column('extra_info', String(200)),
+	)
+unit_table = Table('units',metadata,
+	Column('id', Integer, primary_key=True),
+	Column('coordx', Float),
+	Column('coordy', Float),
+	Column('name', String(50)),
+	Column('timestamp', Float),
+	Column('type', String(50))
+	)
+	
+mission_group = Table('mission_group', metadata,
+	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
+	Column('group_id', None, ForeignKey('group.id'), primary_key=True)
+	)
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
+	
 class User(object):
-	def __init__(self, name=None, clearance=None, group=None, password=None):
+	def __init__(self, name=None, clearance=None,  password=None):
 		self.name=name
 		self.clearance=clearance
-		self.group=group
-		self.password=password
+		self.password=password	
 	def __repr__(self):
-		return "<User('%s','%s','%s', '%s')>" % (self.name, self.clearance, self.group, self.password)
+		return self.name
 class Item(object):
 	def __init__(self, name=None, count=None, location=None):
 		self.name=name
@@ -60,12 +116,85 @@ class Item(object):
 		self.location=location
 	def __repr__(self):
 		return "<Item('%s','%s', '%s')>" % (self.name, self.count, self.location)
+class Group(object):
+	
+	def __init__(self, name=None):
+		self.name=name
+	def __repr__(self):
+		return self.name
+#class Mission(object):
+	#def __init__(self, title=None, body=None, time=None, location=None):
+		#self.title=title
+		#self.body=body
+		#self.time=time
+		#self.location=location
+class mission(object):
+	def __init__(self, poi_id = None, id=None, name= None, timestamp=None, type= None, sub_type= None, description = None, contact_person = None, contact_number = None, status = None, finishtime = None):
+		#self.id = generate_id()
+		self.poi_id = poi_id
+		self.id=id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.contact_person = contact_person
+		self.contact_number = contact_number
+		self.sub_type = sub_type
+		self.description = description
+		self.status = status
+		self.finishtime = finishtime
+class poi(object):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type=None, sub_type= None):
+		self.coordx = coordx
+		self.coordy = coordy
+		self.id = id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.sub_type = sub_type
+	
+		
+class alarm(object):
+	def __init__(self, id=None, name= None, timestamp=None, type= None, poi_id=None, contact_person= None, contact_number= None, extrainfo = None):
+		self.id=id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		self.poi_id=poi_id
+		self.contact_person = contact_person
+		self.contact_number = contact_number
+		self.extrainfo = extrainfo
 
+class unit(object):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type= None):
+		self.coordx = coordx
+		self.coordy = coordy
+		self.id = id
+		self.name = name
+		self.timestamp = timestamp
+		self.type = type
+		
 metadata.create_all(engine)
-mapper(User, users_table)
+mapper(Group, group_table)
+mapper(poi, poi_table)
+mapper(alarm, alarm_table)
+mapper(unit, unit_table)
+
+mapper(mission, mission_table, properties=dict(
+	groups=relation(Group, secondary= mission_group, backref='missions'))
+	)
+mapper(User, user_table, properties=dict(
+	groups=relation(Group, secondary= user_group, backref='users'))
+	)
+
 mapper(Item, items_table)
 
-USERS = session.query(User)
+
+USERS = session.query(User).all()
+
+def get_password(namn):
+	p=session.query(User).filter_by(name=namn).first()
+	return p.password
+
 
 #### --------------------------------------------------------
 
@@ -102,7 +231,7 @@ def atomic_disconnect(client):
 
 	index = search(client)
 	if(index != -1):
-		socketArray[index].socket.send(message)
+		socketArray[index].socket.send('/x')
 		socketArray[index].status = 0
 		sendAll("Server message: " + str(socketArray[index].name) + " disconnected.")
 		socketArray[index].socket.close()
@@ -173,7 +302,8 @@ def statusList():
 
 #HOST = '130.236.218.114'
 HOST = '127.0.0.1'
-PORT = 2150
+#HOST = '130.236.189.22'
+PORT = 2153
 if(len(sys.argv) > 1):
 	PORT = int(sys.argv[1])
 BUFF = 1024
@@ -190,18 +320,21 @@ socketArray = list() # Innehåller alla sockets vi kör
 USERLOGIN = dict()
 USERNAMES = list()
 
-USERS_EXECUTE = USERS.select()
+USERS_EXECUTE = session.query(User).all()
 
 for s in USERS_EXECUTE:
-	#s.execute()
+	##s.execute()
 	USERLOGIN[s.name] = s.password
 	USERNAMES.append(s.name)
 	print s.name
 
 print USERNAMES
-
+print get_password('mathias')
+print session.query(User).all()
+session.commit()
 # Sessionsklassen
 class sessionClass(Thread):
+		
 	groups = list()
 	lastWhisper = "ADMIN"
 	status = 0
@@ -215,11 +348,13 @@ class sessionClass(Thread):
 	def sendBack(self, message):
 		print "Back to " + self.name + ": " + message
 		self.socket.send(message)
-
+	
 	#Sköter inloggningen.
 	def authentication(self):
+			
 		CLIENTNAME = "Player1"
 		try:
+			
 			while 1:
 				self.socket.send("Type a name: ")
 				CLIENTNAME = ""
@@ -233,18 +368,25 @@ class sessionClass(Thread):
 				# Kicka om man inte skriver något efter 20 försök.
 				if(CLIENTNAME == ""):
 					return "/ERROR"
-				if(CLIENTNAME in USERNAMES):
+				if(CLIENTNAME in USERLOGIN):
 					self.socket.send("Type your password " + CLIENTNAME)
 					login = self.socket.recv(BUFF)
+					session= Session()
 					# Atomisk ------
+					
 					ClientMutex.acquire()
+					
 					if(search(CLIENTNAME) == -1):
-						if(login == USERLOGIN[CLIENTNAME]):
+						if(login == get_password(CLIENTNAME)):
 							self.name = CLIENTNAME
+					
 					ClientMutex.release()
+					
 					# --------------
+					session.commit()
 					if(self.name == CLIENTNAME):
 						return CLIENTNAME
+			
 		except Exception, e:
 			print "Client lost: " + CLIENTNAME + " exception: " + str(e)	
 		return "/ERROR"
