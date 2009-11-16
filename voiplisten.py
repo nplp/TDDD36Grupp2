86702f2e877db559770e1763f084bf60f999783a
Niklas Pettersson <nikpe890@gmail.com>
2009-11-16 08:44:50 +0100

    lyssnarn

--- voiplisten.py
+++ voiplisten.py
@@ -30,7 +30,10 @@ class GTK_Main:
 		hbox.add(gtk.Label())
 		window.show_all()
 		#self.player = gst.parse_launch ("v4l2src ! video/x-raw-yuv,width=320,height=240,framerate=8/1 ! udpsink host=192.168.1.112 port=5000")
+		#Connectar
 		self.player = gst.parse_launch ("udpsrc port=5434 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
+		#HOSTAR
+		self.player = gst.parse_launch ("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.216.184 port=5434")
 		#host= host=130.236.216.184
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
