993f6179b4324f1237c5ae70e053c599e2927c9a
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-16 10:39:44 +0100

    ljud3

--- audiorecorder.py
+++ audiorecorder.py
@@ -1,10 +1,8 @@
-import appuifw
+
 import e32
 import audio
 
-audiofile= "e:\audio.wav"
-
-sound=None
+audiofile= "e:\\audio.wav"
 
 def recording():
 	global S
@@ -26,11 +24,37 @@ def closing():
 	s.close()
 	print "stopped"
 	
-def quit():
-	script_lock.signal()
-	appuifw.app.set_exit()
-	
-appuifw.app.menu =[(u"play",playing), (u"record",recording),(u"stop",closing)]
-appuifw.app.exit_key_handler=quit
-script_lock =e32.Ao_lock()
-script_lock.wait()
+
+
+class GTK_Main:
+
+	def __init__(self):
+		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		window.set_title("Awesome Sound")
+		window.set_default_size(500, 400)
+		window.connect("destroy", gtk.main_quit, "WM destroy")
+		vbox = gtk.VBox()
+		window.add(vbox)
+		hbox = gtk.HBox()
+		vbox.pack_start(hbox, False)
+		hbox.set_border_width(10)
+		hbox.pack_start(gtk.Label())
+		self.button = gtk.Button("Record")
+		self.button.connect("clicked", recording())
+		hbox.pack_start(self.button, False)
+		self.button2 = gtk.Button("Quit")
+		self.button2.connect("clicked", self.exit)
+		hbox.pack_start(self.button2, False)
+		hbox.add(gtk.Label())
+		hbox.pack_start(gtk.Label())
+		self.button = gtk.Button("Play")
+		self.button.connect("clicked", playing())
+		hbox.pack_start(self.button, False)
+		window.show_all()
+
+	def exit(self, widget, data=None):
+		gtk.main_quit()
+
+GTK_Main()
+gtk.gdk.threads_init()
+gtk.main()
