9540a288281764e9ed08a59a936426d62f45209d
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-16 09:55:11 +0100

    boob

--- kartkomponent/tupeltest.py
+++ kartkomponent/tupeltest.py
@@ -49,8 +49,8 @@ map.add_object("Shape1", data_storage.MapObject({"longitude":coord[0],
                                                  "latitude":coord[1]},
                                                 "arc(x - 5, y - 5, 10, 0, 2 * math.pi)",
                                                 "set_source_rgb(0, 0, 0)"))
-map.add_object("Tank", data_storage.MapObject({"longitude":coord[0]-0.001,
-						"latitude":coord[1]+0.001},
+map.add_object("Tank", data_storage.MapObject({"longitude":coord[0]-0.0015,
+						"latitude":coord[1]+0.002},
 				                "ikoner/tank.png"))
 
 
