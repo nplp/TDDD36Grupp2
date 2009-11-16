bc527f574c848af6937ebaa0c80f7eaf0cd0efb5
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-16 09:47:37 +0100

    ljud

---
+++ audiorecorder.py
@@ -0,0 +1,36 @@
+import appuifw
+import e32
+import audio
+
+audiofile= "e:\audio.wav"
+
+sound=None
+
+def recording():
+	global S
+	S=audio.Sound.open(audiofile)
+	S.record()
+	print "Recording"	
+def playing():
+	global S
+	try:
+	S=audio.Sound.open(audiofile)
+	S.play()
+	print "playing"
+	except:
+	print "record first"
+	
+def closing():
+	global S
+	S.stop()
+	s.close()
+	print "stopped"
+	
+def quit():
+	script_lock.signal()
+	appuifw.app.set_exit()
+	
+appuifw.app.menu =[(u"play",playing), (u"record",recording),(u"stop",closing)]
+appuifw.app.exit_key_handler=quit
+script_lock =e32.Ao_lock()
+script_lock.wait()
