19fe62380b1b171b19c874c65c946da5266c83b4
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-16 16:01:59 +0100

    jejk

--- osso_test_receiver.py
+++ osso_test_receiver.py
@@ -3,7 +3,9 @@ import osso
 import gtk
  
 def callback_func(interface, method, arguments, user_data, rpc_args):
+	print user_data
 	print rpc_args
+	print rpc_args[0]
 osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
 rpc = osso.Rpc(osso_c)
 rpc.set_rpc_callback("spam.eggs.osso_test_receiver",
@@ -15,3 +17,4 @@ gtk.main()
 
 
     
+wget http://repository.maemo.org/stable/diablo/maemo-scratchbox-install_4.1.2.sh
