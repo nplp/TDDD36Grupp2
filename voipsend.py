587cca1cac90c1a58a697da0bb7a8b843c8e18ef
Niklas Pettersson <nikpe890@gmail.com>
2009-11-16 11:09:32 +0100

    lyssnarn

--- voipsend.py
+++ voipsend.py
@@ -30,10 +30,10 @@ class GTK_Main:
 		hbox.add(gtk.Label())
 		window.show_all()
 		#HOSTAR!
-		self.player = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.122 port=5435")
+		self.player1 = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.122 port=5435")
 		print "skickar video"
 		#CONNECTOR
-		self.player1 = gst.parse_launch("udpsrc port=5434 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
+		self.player = gst.parse_launch("udpsrc port=5434 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
 		print "lyssnar video"
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
