58267ba86820ff32ee70bf5931820221afb133d9
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 10:34:49 +0100

    ford explore-her

--- osso_test_transmitter.py
+++ osso_test_transmitter.py
@@ -8,7 +8,7 @@ def send_rpc(widget, osso_c):
     rpc = osso.Rpc(osso_c)
     rpc.rpc_run("spam.eggs.osso_test_receiver",
                    "/spam/eggs/osso_test_receiver",
-                   "spam.eggs.osso_test_receiver", "do_something",kjtest,wait_reply = False, system_bus = False)
+                   "spam.eggs.osso_test_receiver", "do_something",kjtest,wait_reply = True)
  
 osso_c = osso.Context("osso_test_sender", "0.0.1", False)
  
