88f3ea4d0882161942bfae3d33b8173e4134130f
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 11:37:53 +0100

    hej

--- osso_rec.py
+++ osso_rec.py
@@ -8,3 +8,5 @@ def method1():
 osso_c = osso.Context("receiver", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver", method1, osso_c)
+
+gtk.main()
