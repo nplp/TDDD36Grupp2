15ec0e9254c974996506e02a6a0885bff193e0ee
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 13:43:19 +0100

    hej

--- osso_send.py
+++ osso_send.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python2.5  import osso import hildon import gtk
 import osso
 
 osso_c = osso.Context("sender", "0.0.1", False)
-osso_rpc = osso.Rpc(osso_c)
+#osso_rpc = osso.Rpc(osso_c)
 
-osso_rpc.rpc_run("thor.receiver","/thor/receiver","thor.receiver", "method1",tuple(), wait_reply=True)
+osso_c.rpc_run("thor.receiver","/thor/receiver","thor.receiver", "method1")
+print "RPC sent"
