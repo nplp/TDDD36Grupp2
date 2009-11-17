dd713eecb9936d459b9033ee0a5d7907e1b05b8d
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 14:00:01 +0100

    hej

--- osso_send.py
+++ osso_send.py
@@ -4,6 +4,6 @@ import osso
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 
-osso_rpc.rpc_run("thor.receiver","/thor/receiver","thor.receiver", "method1")
+osso_rpc.rpc_run("thor.receiver","/thor/receiver","thor.receiver", "callback_func")
 print "RPC sent"
 print "sup"
