606b1b48149e82f822328c5e68415ce6c45635f4
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-17 15:59:26 +0100

    voip

--- voipinator2.py
+++ voipinator2.py
@@ -37,53 +37,53 @@ class GTK_Main:
 		#self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! udpsink host=130.236.218.184 port=5000")
 		#videoskicka
 		
-		self.player = gst.parse_launch("udpsrc port=4999 !  ! decodebin name=decoder decoder. ! queue ! audioconvert ! audioresample ! osssink decoder. ! ffmpegcolorspace ! xvimagesink")
+		self.player = gst.parse_launch("udpsrc port=4999 ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! dspilbcsink")
 		#Ljudskicka!
 		#ta bort caps
 		#
-		#self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! udpsink host=130.236.218.184 port=5000")
+		self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! udpsink host=130.236.218.184 port=5000")
 		
-		#self.player2= gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.184 port=5001")
-		#print "skickar video"
-		##Videolyssna
-		#self.player3 = gst.parse_launch("udpsrc port=5002 caps=application/x-rtp, clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
-		#print "lyssnar video"
+		self.player2= gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.184 port=5001")
+		print "skickar video"
+		#Videolyssna
+		self.player3 = gst.parse_launch("udpsrc port=5002 ! decodebin name=decoder decoder. ! queue ! audioconvert ! audioresample ! osssink decoder. ! ffmpegcolorspace ! xvimagesink")
+		print "lyssnar video"
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
 		bus.enable_sync_message_emission()
 		bus.connect("message", self.on_message)
 		bus.connect("sync-message::element", self.on_sync_message)
-		#print "startar bus"
-		#bus1 = self.player1.get_bus()
-		#bus1.add_signal_watch()
-		#bus1.enable_sync_message_emission()
-		#bus1.connect("message", self.on_message)
-		#bus1.connect("sync-message::element", self.on_sync_message)
-		#print "startar bus1"
-		#bus2 = self.player2.get_bus()
-		#bus2.add_signal_watch()
-		#bus2.enable_sync_message_emission()
-		#bus2.connect("message", self.on_message)
-		#bus2.connect("sync-message::element", self.on_sync_message)
-		#print "startar bus2"
-		#bus3 = self.player3.get_bus()
-		#bus3.add_signal_watch()
-		#bus3.enable_sync_message_emission()
-		#bus3.connect("message", self.on_message)
-		#bus3.connect("sync-message::element", self.on_sync_message)
-		#print "startar bus3"
+		print "startar bus"
+		bus1 = self.player1.get_bus()
+		bus1.add_signal_watch()
+		bus1.enable_sync_message_emission()
+		bus1.connect("message", self.on_message)
+		bus1.connect("sync-message::element", self.on_sync_message)
+		print "startar bus1"
+		bus2 = self.player2.get_bus()
+		bus2.add_signal_watch()
+		bus2.enable_sync_message_emission()
+		bus2.connect("message", self.on_message)
+		bus2.connect("sync-message::element", self.on_sync_message)
+		print "startar bus2"
+		bus3 = self.player3.get_bus()
+		bus3.add_signal_watch()
+		bus3.enable_sync_message_emission()
+		bus3.connect("message", self.on_message)
+		bus3.connect("sync-message::element", self.on_sync_message)
+		print "startar bus3"
 	def start_stop(self, w):
 		if self.button.get_label() == "Start":
 			self.button.set_label("Stop")
 			self.player.set_state(gst.STATE_PLAYING)
-			#self.player1.set_state(gst.STATE_PLAYING)
-			#self.player2.set_state(gst.STATE_PLAYING)
-			#self.player3.set_state(gst.STATE_PLAYING)
+			self.player1.set_state(gst.STATE_PLAYING)
+			self.player2.set_state(gst.STATE_PLAYING)
+			self.player3.set_state(gst.STATE_PLAYING)
 		else:
 			self.player.set_state(gst.STATE_NULL)
-			#self.player1.set_state(gst.STATE_NULL)
-			#self.player2.set_state(gst.STATE_NULL)
-			#self.player3.set_state(gst.STATE_NULL)
+			self.player1.set_state(gst.STATE_NULL)
+			self.player2.set_state(gst.STATE_NULL)
+			self.player3.set_state(gst.STATE_NULL)
 			self.button.set_label("Start")
 
 	def exit(self, widget, data=None):
@@ -93,17 +93,17 @@ class GTK_Main:
 		t = message.type
 		if t == gst.MESSAGE_EOS:
 			self.player.set_state(gst.STATE_NULL)
-			#self.player1.set_state(gst.STATE_NULL)
-			#self.player2.set_state(gst.STATE_NULL)
-			#self.player3.set_state(gst.STATE_NULL)
+			self.player1.set_state(gst.STATE_NULL)
+			self.player2.set_state(gst.STATE_NULL)
+			self.player3.set_state(gst.STATE_NULL)
 			self.button.set_label("Start")
 		elif t == gst.MESSAGE_ERROR:
 			err, debug = message.parse_error()
 			print "Error: %s" % err, debug
 			self.player.set_state(gst.STATE_NULL)
-			#self.player1.set_state(gst.STATE_NULL)
-			#self.player2.set_state(gst.STATE_NULL)
-			#self.player3.set_state(gst.STATE_NULL)
+			self.player1.set_state(gst.STATE_NULL)
+			self.player2.set_state(gst.STATE_NULL)
+			self.player3.set_state(gst.STATE_NULL)
 			self.button.set_label("Start")
 
 	def on_sync_message(self, bus, message):
