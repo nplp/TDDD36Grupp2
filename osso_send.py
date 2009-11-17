3349501b3b3e0d1e3d85b15b820ecbdf85634f29
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 14:13:36 +0100

    hej

--- osso_send.py
+++ osso_send.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2.5
 import osso
-import hildon
 import gtk
+
+osso_c = osso.Context("sender", "0.0.1", False)
 
 def send_rpc(widget, osso_c):
     osso_rpc = osso.Rpc(osso_c)
@@ -10,13 +11,9 @@ def send_rpc(widget, osso_c):
         "thor.receiver", "do_something")
     print "RPC sent"
 
-osso_c = osso.Context("sender", "0.0.1", False)
-window = hildon.Window()
-window.connect("destroy", gtk.main_quit)
-send_button = gtk.Button("Send RPC")
-window.add(send_button)
-send_button.connect("clicked", send_rpc, osso_c)
-window.show_all()
+
+send_rpc()
+
 gtk.main()
 
 
