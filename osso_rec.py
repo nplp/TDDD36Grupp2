3fb8b6a71076ccf8eec2f4b00c4314b528d48f37
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 15:13:01 +0100

    hej

--- osso_rec.py
+++ osso_rec.py
@@ -1,3 +1,27 @@
+import osso
+import gtk
+
+rpc.register_name("receiver")
+rpc.receive(receiver)
+gtk.main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 #!/usr/bin/python2.5
 import osso
 import gtk
@@ -15,53 +39,8 @@ osso_rpc = osso.Rpc(osso_c)
 osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver",metoden)
 
 gtk.main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 '''
-import osso
-import gtk
 
-def callback_func():
-	print "har hander det grejer!"
-	
 
-osso_c = osso.Context("receiver", "0.0.1", False)
-osso_rpc = osso.Rpc(osso_c)
-osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver", callback_func, osso_c)
 
-gtk.main()
-'''
+
