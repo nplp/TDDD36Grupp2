4af7e8a715747b13f373565a58f90336d532e079
Niklas Pettersson <nikpe890@gmail.com>
2009-11-17 11:25:13 +0100

    wtf test hille

--- voipinator1.py
+++ voipinator1.py
@@ -34,10 +34,10 @@ class GTK_Main:
 		#Ljudskicka!
 		self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! udpsink host=130.236.218.186 port=4999")
 		#videoskicka
-		self.player2= gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! udpsink host=130.236.218.186 port=5002")
+		self.player2= gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.218.186 port=5002")
 		print "skickar video"
 		#Videolyssna
-		self.player3 = gst.parse_launch("udpsrc port=5001 caps=application/x-rtp,clock-rate=90000 ! hantro4100dec ! xvimagesink")
+		self.player3 = gst.parse_launch("udpsrc port=5001 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
 		print "lyssnar video"
 		bus = self.player.get_bus()
 		bus.add_signal_watch()
