40303c4a4c7e452df2b9a8fff0cca330b5b42328
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 11:31:07 +0100

    hej

---
+++ osso_rec.py
@@ -0,0 +1,10 @@
+import osso
+import gtk
+
+def method1():
+	print "här händer det grejer!"
+	
+
+osso_c = osso.Context("receiver", "0.0.1", False)
+osso_rpc = osso.Rpc(osso_c)
+osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver", method1, osso_c)
