988b4b4e85e63590cee89aed3feb3474082792a9
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 11:58:01 +0100

    hej

--- osso_send.py
+++ osso_send.py
@@ -4,5 +4,5 @@ osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 try:
 	osso_rpc.rpc_run("thor.receiver","/thor/receiver","thor.receiver", "method1",tuple(), wait_reply=True)
-except(Exception e):
+except(Error, e):
 	print e
