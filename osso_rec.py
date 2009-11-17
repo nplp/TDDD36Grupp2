9c821f40b88863185200364aa39760958ee9b1aa
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-17 14:08:32 +0100

    hej

--- osso_rec.py
+++ osso_rec.py
@@ -1,20 +1,21 @@
- #!/usr/bin/python2.5
- import osso
- import gtk
+#!/usr/bin/python2.5
+import osso
+import gtk
  
- def callback_func(interface, method, arguments, user_data):
-     print "RPC received"
-     osso_c = user_data
-     osso_sysnote = osso.SystemNote(osso_c)
-     osso_sysnote.system_note_infoprint("osso_test_receiver: Received an RPC to %s." % method)
- 
- osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
- print "osso_test_receiver started"
- osso_rpc = osso.Rpc(osso_c)
- osso_rpc.set_rpc_callback("spam.eggs.osso_test_receiver",
-     "/spam/eggs/osso_test_receiver",
-     "spam.eggs.osso_test_receiver", callback_func, osso_c)
- gtk.main()
+def callback_func(interface, method, arguments, user_data):
+    print "RPC received"
+    osso_c = user_data
+    osso_sysnote = osso.SystemNote(osso_c)
+    osso_sysnote.system_note_infoprint("osso_test_receiver: Received an RPC to %s." % method)
+
+osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
+print "osso_test_receiver started"
+
+osso_rpc = osso.Rpc(osso_c)
+
+osso_rpc.set_rpc_callback("spam.eggs.osso_test_receiver","/spam/eggs/osso_test_receiver","spam.eggs.osso_test_receiver",callback_func, osso_c)
+
+gtk.main()
 
 
 
