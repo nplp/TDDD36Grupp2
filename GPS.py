30f06daf6d4416c33239ce607da128a833c188d5
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-18 09:55:53 +0100

    jobigt

--- GPS.py
+++ GPS.py
@@ -4,33 +4,33 @@ import gpsbt
 import thread
 
 
-class GPS:
+class GPS(object):
 	
-	self.__coord = (0,0)
-	self.__update == False
-	self.__con
+	coord = (0,0)
+	update = False
+	con
 
 	# Uppdaterar din kordinat
 	def updatecoord():
 		print "uppdaterar"
-		self.__coord = gps.get_position()
+		coord = gps.get_position()
 	 
 	# Väntar på att gpsen ska hitta en kordinat
 	def waiting_for_a_fix():
 		i = 0
 		print "Vi vantar pa en koordinat"
 		#global coord
-		while (self.__coord == (0,0)):
-			self.__coord = gps.get_position()
+		while (coord == (0,0)):
+			coord = gps.get_position()
 			print "Waiting: "+ str(i)
 			i+=1
 			time.sleep(2)
 
 	def send_coordinates():
-		self.__update == True
-		while (self.__update == True):
-			print self.__coord[0]
-			print self.__coord[1]			
+		update = True
+		while (update == True):
+			print coord[0]
+			print coord[1]			
 			time.sleep(5)
 			try:			
 				updatecoord()
@@ -39,7 +39,7 @@ class GPS:
 		
 
 	# Startar GPSEN
-	self.__con = gpsbt.start()
+	con = gpsbt.start()
 	time.sleep(2.0) # wait for gps to come up
 	 
 	#Getting GPS coordinats
