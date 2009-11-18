b75f4c6fbcadf16ce8dbcb9717ff2079e64dd742
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 15:32:04 +0100

    commit by HAnna Montana

--- osso_send.py
+++ osso_send.py
@@ -12,9 +12,6 @@ osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "method2", args)
 
-def h:
-	print "tjabba"
-
 
 '''
 #!/usr/bin/python2.5
