1a5f2311054a0b50f725dcc7a7017c946c28684d
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 15:35:15 +0100

    commit by HAnna Montana

--- osso_send.py
+++ osso_send.py
@@ -6,11 +6,11 @@ args = ("le tupel!",)
 #################################
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
-osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "method1", args)
+osso_rpc.rpc_run(".receiver", "/receiver", ".receiver", "method1", args)
 #################################
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
-osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "method2", args)
+osso_rpc.rpc_run(".receiver", "/receiver", ".receiver", "method2", args)
 
 
 '''
