1d169e13259f49da75cb288bbbe17641c3f3b923
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-18 10:44:03 +0100

    ll cool j

--- GPS.py
+++ GPS.py
@@ -3,50 +3,46 @@ import time
 import gpsbt
 import thread
 
-class GPS(object):
-	
-	coord = (0,0)
-	update = False
+coord = (0,0)
+update = False
 
-	# Uppdaterar din kordinat
-	def updatecoord(self):
-		print "uppdaterar"
+# Uppdaterar din kordinat
+def updatecoord(self):
+	print "uppdaterar"
+	self.coord = gps.get_position()
+	 
+# Väntar på att gpsen ska hitta en kordinat
+def waiting_for_a_fix(self):
+	i = 0
+	print "Vi vantar pa en koordinat"
+	while (self.coord == (0,0)):
 		self.coord = gps.get_position()
+		print "Waiting: "+ str(i)
+		i+=1
+		time.sleep(2)
+
+def send_coordinates(self):
+	self.update = True
+	while (self.update == True):
+		print self.coord[0]
+		print self.coord[1]
+		time.sleep(5)
+		try:
+			self.updatecoord()
+		except:
+			gpsbt.stop(__con)
+	
+
+# Startar GPSEN
+con = gpsbt.start()
+time.sleep(2.0) # wait for gps to come up
 	 
-	# Väntar på att gpsen ska hitta en kordinat
-	def waiting_for_a_fix(self):
-		i = 0
-		print "Vi vantar pa en koordinat"
-		while (self.coord == (0,0)):
-			self.coord = gps.get_position()
-			print "Waiting: "+ str(i)
-			i+=1
-			time.sleep(2)
-
-	def send_coordinates(self):
-		self.update = True
-		while (self.update == True):
-			print self.coord[0]
-			print self.coord[1]
-			time.sleep(5)
-			try:
-				self.updatecoord()
-			except:
-				gpsbt.stop(__con)
-		
-
-	# Startar GPSEN
-	con = gpsbt.start()
-	time.sleep(2.0) # wait for gps to come up
+#Getting GPS coordinats
+gps = gpsbt.gps()
 	 
-	#Getting GPS coordinats
-	gps = gpsbt.gps()
-	 
-	#Vantar pa en gps koordinat
-	print "Waiting baby"
-	self.waiting_for_a_fix()
+#Vantar pa en gps koordinat
+print "Waiting baby"
+self.waiting_for_a_fix()
+self.send_coordinates()
 
-	self.send_coordinates()
-
-GPS()
 
