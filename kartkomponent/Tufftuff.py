041e51857e276c25dba83c814f8d30acfa20cff7
Krisse <krisse@ubuntu.ubuntu-domain>
2009-11-18 15:35:38 +0100

    tufftuff

---
+++ kartkomponent/Tufftuff.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import time
+import thread
+
+class GPS(object):
+
+	def __init__(self):
+		self.coord = (0,0)
+		self.update = False
+		self.x = 30
+		self.y = 40
+
+	# Uppdaterar din kordinat
+	def updatecoord(self):
+		print "uppdaterar"
+		self.coord = (self.x,self.y)
+		self.x += 1
+		self.y += 1
+		return self.coord
+	 
+	# Väntar på att gpsen ska hitta en kordinat
+	def waiting_for_a_fix(self):
+		i = 0
+		print "Vi vantar pa en koordinat"
+		self.coord = (5,10)
+		print "Waiting: "+ str(i)
+		
+	def send_coordinates(self):
+		self.update = True
+		while (self.update == True):
+			print self.coord[0]
+			print self.coord[1]
+			time.sleep(5)
+			#try:
+			self.updatecoord()
+			#except:
+			#	gpsbt.stop(self.con)
+	
+	def run(self):
+		# Vantar pa en gps koordinat
+		print "Waiting baby"
+		self.waiting_for_a_fix()
+
+		self.send_coordinates()
+
+
+def main():
+    gtk.main()
+
+if __name__ == "__main__":
+    GPS().run()
+    main()
+
+
