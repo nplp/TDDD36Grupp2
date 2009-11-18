bc433c090229983118b5cf772d7c69c427348160
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-18 16:41:50 +0100

    hej

--- kartkomponent/battery.py
+++ kartkomponent/battery.py
@@ -1,8 +1,7 @@
 import dbus
 
 bus = dbus.SystemBus()
-hal_obj = bus.get_object ('org.freedesktop.Hal', 
-                          #'/org/freedesktop/Hal/Manager')
+hal_obj = bus.get_object ('org.freedesktop.Hal','/org/freedesktop/Hal/Manager')
 hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 uids = hal.FindDeviceByCapability('battery')
 dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
