6029f73a1c45911f6268c29a7d6591abc5e43ac4
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 09:19:06 +0100

    Paris Hildon

---
+++ Dbus_registration.py
@@ -0,0 +1,20 @@
+import osso
+
+osso_rpc = None
+name = None
+
+class Dbus_registration():
+	__init__(self, name):
+		self.name = name
+		register_name(name)
+
+	def register_name(newname):
+		self.name = newname
+		osso_c = osso.Context(newname, "0.0.1", False)
+		self.osso_rpc = osso.Rpc(osso_c)
+	
+	def send_rpc(receiver, method, message):
+		osso_rpc.rpc_run("thor."+receiver, "/thor/"+receiver, "thor."+receiver, method, (message,))
+	
+	def receive(metoden):
+		self.osso_rpc.set_rpc_callback("thor."+name,"/thor/"+name,"thor."+name, metoden)
