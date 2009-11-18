dd82e30988910df14dc151626577ef46c2f181fb
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 16:32:13 +0100

    commit by HAnna Montana

--- osso_send.py
+++ osso_send.py
@@ -1,13 +1,14 @@
 import osso
 import gtk
 
+#args = de argument som ska skickas som tupel
 args = ("le tupel!",)
 
-#################################
+################################# anropar metod 1 i processen med registrerat namn receiver
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "method1", args)
-#################################
+################################# anropar metod 2 i processen med registrerat namn receiver
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
 osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "method2", args)
