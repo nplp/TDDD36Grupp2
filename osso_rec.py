8db3658ad49e2112f9b769ebe6af61a95c5385cc
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 14:34:39 +0100

    commit by HAnna Montana

--- osso_rec.py
+++ osso_rec.py
@@ -1,50 +1,27 @@
-#!/usr/bin/python2.5
-import osso
-import gtk
-from rpc import *
- 
-def metoden(interface, method, arguments, user_data):
-    print "RPC received"
-    print arguments[0]
-
-
-rpc.register_name("receiver")
-
-
-rpc.osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver",metoden)
-
-gtk.main()
-
-
-
-
-
-
-
-
-
-
-
-'''
-#!/usr/bin/python2.5
 import osso
 import gtk
  
-def metoden(interface, method, arguments, user_data):
-    print "RPC received"
-    print arguments[0]
-
-
+def callback_func(interface, method, arguments, user_data):
+    if method = "medthod1"
+    	method1(arguments[0])
+    elif method = "method2"
+	method2(arguments[0])
+	
+########################
 osso_c = osso.Context("receiver", "0.0.1", False)
-print "osso_test_receiver started"
-
 osso_rpc = osso.Rpc(osso_c)
-
-osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver",metoden)
+osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver",callback_func)
+########################
 
 gtk.main()
-'''
 
 
+def method1(arg):
+	print "här printas det saker fran metod 1"
+	print arg
+	
+def method2(arg):
+	print "här printas det saker fran metod 2"
+	print arg
 
 
--- osso_send.py
+++ osso_send.py
@@ -1,45 +1,13 @@
 import osso
 import gtk
-from Dbus_registration import *
-
-rpc = Dbus_registration("sender")
-
-rpc.send_rpc("receiver", "metod33", "le message!!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-#!/usr/bin/python2.5
-import osso
-import gtk
 
 args = ("le tupel!",)
+
+#################################
 osso_c = osso.Context("sender", "0.0.1", False)
 osso_rpc = osso.Rpc(osso_c)
-osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "metod", args)
-print "RPC sent"
-'''
+osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "metod1", args)
+#################################
+osso_c = osso.Context("sender", "0.0.1", False)
+osso_rpc = osso.Rpc(osso_c)
+osso_rpc.rpc_run("thor.receiver", "/thor/receiver", "thor.receiver", "metod2", args)
