8d9d7db61cad4eea72cf3c7c9c633c3375ab358d
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-18 16:36:26 +0100

    hej

--- kartkomponent/battery.py
+++ kartkomponent/battery.py
@@ -1,17 +1,14 @@
-#import dbus
+import dbus
 
-#bus = dbus.SystemBus()
-#hal_obj = bus.get_object ('org.freedesktop.Hal', 
+bus = dbus.SystemBus()
+hal_obj = bus.get_object ('org.freedesktop.Hal', 
                           #'/org/freedesktop/Hal/Manager')
-#hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
-#uids = hal.FindDeviceByCapability('battery')
-#dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
+hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
+uids = hal.FindDeviceByCapability('battery')
+dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
 
-#x = float(dev_obj.GetProperty('battery.reporting.current'))
-#y = float(dev_obj.GetProperty('battery.reporting.design'))
-
-x = 40
-y = 4
+x = float(dev_obj.GetProperty('battery.reporting.current'))
+y = float(dev_obj.GetProperty('battery.reporting.design'))
 
 batteryprint = int((x/y)*100),'%'
 #x2 = float(dev_obj.GetProperty('battery.voltage.current'))
