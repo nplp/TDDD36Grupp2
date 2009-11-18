4d9489e913b3cd5d24e97ccb3deb275e0364bfbd
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 11:02:23 +0100

    Paris Hildon

--- osso_rec.py
+++ osso_rec.py
@@ -9,7 +9,7 @@ def metoden(interface, method, arguments, user_data):
 print "ja"
 rpc = Dbus_registration("receiver")
 print "visst"
-rpc.receive(metoden)
+rpc.osso_rpc.set_rpc_callback("thor.receiver" ,"/thor/receiver" ,"thor.receiver", metoden)
 gtk.main()
 
 
