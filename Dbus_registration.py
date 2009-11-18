dab67b1c195fe70beecc20b70c82aca1c8b8a431
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 13:42:01 +0100

    commit by HAnna Montana

--- Dbus_registration.py
+++ Dbus_registration.py
@@ -19,7 +19,5 @@ class Dbus_registration():
 	
 	def receive(self,metoden):
 		print "bajs"
-		if(self.osso_rpc == None):
-			print "det stod None har ojsan2"
-		self.osso_rpc.set_rpc_callback("thor.receiver" ,"/thor/receiver" ,"thor.receiver", metoden)
+		self.osso_rpc.set_rpc_callback("thor.receiver" ,"/thor/receiver" ,"thor.receiver", self.metoden)
 		print "pa dig"
