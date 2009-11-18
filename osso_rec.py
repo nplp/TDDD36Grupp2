42c0360e2039899aeb8a0d4b32d4a156e87655b3
Thor Bossuyt <thobo260@sysi-15.sysinst.ida.liu.se>
2009-11-18 14:40:22 +0100

    commit by HAnna Montana

--- osso_rec.py
+++ osso_rec.py
@@ -2,10 +2,11 @@ import osso
 import gtk
  
 def callback_func(interface, method, arguments, user_data):
-    if(method == "method1"):
-    	method1(arguments[0])
-    elif(method == "method2"):
-	method2(arguments[0])
+	print "rpc received"
+	if(method == "method1"):
+    		method1(arguments[0])
+	elif(method == "method2"):
+		method2(arguments[0])
 	
 ########################
 osso_c = osso.Context("receiver", "0.0.1", False)
@@ -24,6 +25,24 @@ def method2(arg):
 gtk.main()
 
 
+'''
+#!/usr/bin/python2.5
+import osso
+import gtk
+ 
+def metoden(interface, method, arguments, user_data):
+    print "RPC received"
+    print arguments[0]
 
+
+osso_c = osso.Context("receiver", "0.0.1", False)
+print "osso_test_receiver started"
+
+osso_rpc = osso.Rpc(osso_c)
+
+osso_rpc.set_rpc_callback("thor.receiver","/thor/receiver","thor.receiver",metoden)
+
+gtk.main()
+'''
 
 
