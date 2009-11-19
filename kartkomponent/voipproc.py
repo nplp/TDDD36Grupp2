d6e743afa9610f280892897f0a9f9b1ca90ec5a5
Niklas Pettersson <nikpe890@gmail.com>
2009-11-19 15:25:50 +0100

    ZAZOOOOgit add voipproc.py

--- kartkomponent/voipproc.py
+++ kartkomponent/voipproc.py
@@ -11,8 +11,8 @@ class Mainstream:
 	def __init__(self):
 		self.choose = 0
 		self.HOSTIP = '127.0.0.1'
-		self.MYPORT = '5000'
-		self.HOSTPORT = '5000'
+		self.MYPORT = 5000
+		self.HOSTPORT = 5000
 	def run(self, choose, HOSTIP, MYPORT, HOSTPORT):
 		self.choose = choose
 		self.HOSTIP = HOSTIP
@@ -44,8 +44,8 @@ class Mainstream:
 			print self.HOSTIP
 			print self.MYPORT
 			print self.HOSTPORT
-			self.player = gst.parse_launch("udpsrc port="+self.MYPORT+" ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! dspilbcsink")
-			self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20  ! udpsink host="+self.HOSTIP+" port= "+self.HOSTPORT)
+			self.player = gst.parse_launch("udpsrc port="+str(self.MYPORT)+" ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! dspilbcsink")
+			self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20  ! udpsink host="+self.HOSTIP+" port= "+str(self.HOSTPORT))
 			bus = self.player.get_bus()
 			bus.add_signal_watch()
 			bus.enable_sync_message_emission()
@@ -58,8 +58,9 @@ class Mainstream:
 			bus1.connect("sync-message::element", self.on_sync_message)
 		#Videosamtal = 2
 		elif (self.choose == 2):
-			self.player2 = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host="+self.HOSTIP+" port="+self.HOSTPORT)
-			self.player3 = gst.parse_launch("udpsrc port="+self.MYPORT+" caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
+			print "kor jag tvaan?"
+			self.player2 = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host="+self.HOSTIP+" port="+str(self.HOSTPORT))
+			self.player3 = gst.parse_launch("udpsrc port="+str(self.MYPORT)+" caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
 			bus2 = self.player2.get_bus()
 			bus2.add_signal_watch()
 			bus2.enable_sync_message_emission()
@@ -72,10 +73,11 @@ class Mainstream:
 			bus3.connect("sync-message::element", self.on_sync_message)
 		#Videorostsamtal = 3
 		elif (self.choose == 3):
-			self.player = gst.parse_launch("udpsrc port="+self.MYPORT+" ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! dspilbcsink")
-			self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20  ! udpsink host="+self.HOSTIP+" port= "+self.HOSTPORT)
-			self.player2 = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host="+self.HOSTIP+" port="+self.HOSTPORT)
-			self.player3 = gst.parse_launch("udpsrc port="+self.MYPORT+" caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
+			print "kor jag trean?"
+			self.player = gst.parse_launch("udpsrc port="+str(self.MYPORT)+" ! audio/x-iLBC,rate=8000,channels=1,mode=20 ! dspilbcsink")
+			self.player1 = gst.parse_launch("dspilbcsrc dtx=0 ! audio/x-iLBC,rate=8000,channels=1,mode=20  ! udpsink host="+self.HOSTIP+" port= "+str(self.HOSTPORT))
+			self.player2 = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host="+self.HOSTIP+" port="+str(self.HOSTPORT))
+			self.player3 = gst.parse_launch("udpsrc port="+str(self.MYPORT)+" caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink")
 			bus = self.player.get_bus()
 			bus.add_signal_watch()
 			bus.enable_sync_message_emission()
