b6b022a352de7939206c02cfbbfc6bbcdd8cfdf1
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-19 09:31:42 +0100

    hej

--- kartkomponent/battery.py
+++ kartkomponent/battery.py
@@ -1,26 +1,29 @@
-import dbus
-import thread
-import time
+#import dbus
+#import thread
+#import time
 
-def listenBattery():
-	while(1):
-		x = float(dev_obj.GetProperty('battery.reporting.current'))
-		y = float(dev_obj.GetProperty('battery.reporting.design'))
-		time.sleep(5)
+#def listenBattery():
+	#while(1):
+		#x = float(dev_obj.GetProperty('battery.reporting.current'))
+		#y = float(dev_obj.GetProperty('battery.reporting.design'))
+		#time.sleep(5)
 
-bus = dbus.SystemBus()
-hal_obj = bus.get_object ('org.freedesktop.Hal', 
-                          '/org/freedesktop/Hal/Manager')
-hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
-uids = hal.FindDeviceByCapability('battery')
-dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
+#bus = dbus.SystemBus()
+#hal_obj = bus.get_object ('org.freedesktop.Hal', 
+                          #'/org/freedesktop/Hal/Manager')
+#hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
+#uids = hal.FindDeviceByCapability('battery')
+#dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
 
-x = float(dev_obj.GetProperty('battery.reporting.current'))
-y = float(dev_obj.GetProperty('battery.reporting.design'))
+#x = float(dev_obj.GetProperty('battery.reporting.current'))
+#y = float(dev_obj.GetProperty('battery.reporting.design'))
+
+x=50
+y=10
 
 batteryprint = int((x/y)*100),'%'
-#x2 = float(dev_obj.GetProperty('battery.voltage.current'))
-#y2 = float(dev_obj.GetProperty('battery.voltage.design'))
-#print 'usage level', int((x2/y2)*100),'%'
+##x2 = float(dev_obj.GetProperty('battery.voltage.current'))
+##y2 = float(dev_obj.GetProperty('battery.voltage.design'))
+##print 'usage level', int((x2/y2)*100),'%'
 	
-thread.start_new_thread(listenBattery,())
+#thread.start_new_thread(listenBattery,())
