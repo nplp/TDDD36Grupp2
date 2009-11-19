4b5b5d33e1c5affe982679561fca9657aa22651b
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-19 16:41:42 +0100

    hanna

--- kartkomponent/battery.py
+++ kartkomponent/battery.py
@@ -1,23 +1,29 @@
-import dbus
+#import dbus
 import thread
 import time
  
 class Batteri():
     
     def __init__(self):
+	'''
 	self.bus = dbus.SystemBus()
 	self.hal_obj = self.bus.get_object ('org.freedesktop.Hal', '/org/freedesktop/Hal/Manager')
 	self.hal = dbus.Interface (self.hal_obj, 'org.freedesktop.Hal.Manager')
 	self.uids = self.hal.FindDeviceByCapability('battery')
 	self.dev_obj = self.bus.get_object ('org.freedesktop.Hal', uids[0])
  
-	#self.x = float(self.dev_obj.GetProperty('battery.reporting.current'))
-	#self.y = float(self.dev_obj.GetProperty('battery.reporting.design'))
- 
-	self.batterylevel = (0,0)
+	self.x = float(self.dev_obj.GetProperty('battery.reporting.current'))
+	self.y = float(self.dev_obj.GetProperty('battery.reporting.design'))
+	'''
+	self.batterylevel = 0
+	self.x = 100
+	self.y = 100
 	#x2 = float(dev_obj.GetProperty('battery.voltage.current'))
 	#y2 = float(dev_obj.GetProperty('battery.voltage.design'))
 	#print 'usage level', int((x2/y2)*100),'%'
+ 
+    def bluff(self):
+    	self.x -= 1
  
  
     def getbattery(self):
@@ -27,9 +33,10 @@ class Batteri():
  
     def listenBattery(self):
 	while(1):
-		self.batterylevel[0] = float(self.dev_obj.GetProperty('battery.reporting.current'))
-		self.batterylevel[1] = float(self.dev_obj.GetProperty('battery.reporting.design'))
-		time.sleep(60)
+		self.bluff()
+		#self.x = float(self.dev_obj.GetProperty('battery.reporting.current'))
+		#self.y = float(self.dev_obj.GetProperty('battery.reporting.design'))
+		time.sleep(5)
  
     def run(self):
 	thread.start_new_thread(self.listenBattery,())
