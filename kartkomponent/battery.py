52f0db606a0403b6f89f4b453e2da0b80192117f
Hanna Germundsson <hange595@sysi-09.sysinst.ida.liu.se>
2009-11-19 08:46:00 +0100

    hej

--- kartkomponent/battery.py
+++ kartkomponent/battery.py
@@ -1,7 +1,16 @@
 import dbus
+import thread
+import time
+
+def listenBattery(self):
+	while(1):
+		x = float(dev_obj.GetProperty('battery.reporting.current'))
+		y = float(dev_obj.GetProperty('battery.reporting.design'))
+		time.sleep(5)
 
 bus = dbus.SystemBus()
-hal_obj = bus.get_object ('org.freedesktop.Hal','/org/freedesktop/Hal/Manager')
+hal_obj = bus.get_object ('org.freedesktop.Hal', 
+                          #'/org/freedesktop/Hal/Manager')
 hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 uids = hal.FindDeviceByCapability('battery')
 dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
@@ -13,4 +22,5 @@ batteryprint = int((x/y)*100),'%'
 #x2 = float(dev_obj.GetProperty('battery.voltage.current'))
 #y2 = float(dev_obj.GetProperty('battery.voltage.design'))
 #print 'usage level', int((x2/y2)*100),'%'
-
+	
+thread.start_new_thread(self.listenBattery,())
