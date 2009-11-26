439c31e3846475306923828a6630ae0873882b3f
HanaMontana <hange595@sysi-09.(none)>
2009-11-26 12:54:12 +0000

    hej

--- kartkomponent/lager.py
+++ kartkomponent/lager.py
@@ -14,18 +14,15 @@ class Lager():
     def __init__(self):	
 	temp=get_item_all()
 	combobox = gtk.combo_box_new_text()
-	#combobox.set_size_request(300,50)
+	#combobox.set_size_request(30,300)
 	
 	for n in temp:
 		combobox.insert_text(n.item_id, (n.name+" "+str (n.count)+" "+n.location))
 
 	
-	self.lagerboxen = gtk.HBox(True, 0)
-	#cell = gtk.CellRendererText()
-  	#combobox.pack_start(cell, True)
-  	#combobox.add_attribute(cell, 'text', 0)
-	self.lagerboxen.pack_start(combobox, True, True, 0)
-	self.lagerboxen.set_size_request(50, 50)
+	self.lagerboxen = gtk.VBox(False, 0)
+	self.lagerboxen.pack_start(combobox, False, False, 0)
+	#self.lagerboxen.set_size_request(50, 250)
 	
 	combobox.show()
 
