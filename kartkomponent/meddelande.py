b7891138d7020cc5d8223ba7509f37bb58ef6fde
Manuela Tesanovic <manha686@sysi-02.sysinst.ida.liu.se>
2009-11-26 14:22:33 +0100

    new meddelande

--- kartkomponent/meddelande.py
+++ kartkomponent/meddelande.py
@@ -12,7 +12,8 @@ class Meddelande (object):
 	    
 	#Vbox for innehall
 	self.vbox = gtk.VBox(False,5)
-	self.vbox.set_border_width(50)	
+	self.vbox.set_border_width(50)
+	self.vbox.show()	
 	
 	self.scrolled_window=gtk.ScrolledWindow()
 	self.scrolled_window.set_border_width(10)
@@ -25,13 +26,28 @@ class Meddelande (object):
 	self.amne.show()
 	self.vbox.pack_start(self.amne, False, False, 0)
 	
-	
 	self.entry = gtk.Entry()
         self.entry.set_max_length(250)
 	self.entry.show()	
 	self.vbox.pack_start(self.entry, True, True, 0)
-
 	
+	#Skriv ett amne
+        self.mottagare = gtk.Label("Mottagare")
+        self.mottagare.set_alignment(0, 0)
+	self.mottagare.show()
+	self.vbox.pack_start(self.mottagare , False, False, 0)
+	
+	self.entry1 = gtk.Entry()
+        self.entry1.set_max_length(250)
+	self.entry1.show()	
+	self.vbox.pack_start(self.entry1, True, True, 0)
+	
+	#Lagg till mottagre
+	self.skicka1 = gtk.Button("Lagg till mottagare")
+        self.skicka1.connect("clicked", self.send, "Lagg till mottagare")
+	self.skicka1.show()
+	self.vbox.pack_start(self.skicka1,True,True,0)
+
 	#Skriv ett meddelande
         self.meddelande = gtk.Label("Meddelande")
         self.meddelande.set_alignment(0, 0)
@@ -50,13 +66,3 @@ class Meddelande (object):
 	self.skicka.show()
 	self.vbox.pack_start(self.skicka,True,True,0)
 	
-def main():
-	gtk.main()
-	return 0
-
-if __name__ == "__main__":
-    Meddelande()
-    main()
-
-	
-
