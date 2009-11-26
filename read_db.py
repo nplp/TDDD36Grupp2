1e3f623961b9700f01c9ab0c14d36b257c9e4997
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-26 14:33:33 +0100

    nya typer i databasen

--- read_db.py
+++ read_db.py
@@ -14,21 +14,21 @@ metadata = MetaData()
 #######################skapar data tabeller############################33
 group_table = Table('group', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(20))
+	Column('name', Text)
 	)
 	
 user_table = Table('users', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(20)),
-	Column('clearance', String(40)),
-	Column('password', String(40))
+	Column('name', Text),
+	Column('clearance', Text),
+	Column('password', Text)
 	)
 	
 items_table = Table('items_table', metadata,
 	Column('item_id', Integer, primary_key=True),
-	Column('name', String(40)),
+	Column('name', Text),
 	Column('count', Integer),
-	Column('location', String(40))
+	Column('location', Text)
 	)
 
 user_group = Table('user_group', metadata,
@@ -37,54 +37,66 @@ user_group = Table('user_group', metadata,
 	)
 	
 mission_table =Table('missions', metadata,
+	Column('poi_ids', Integer),
+	Column('unit_ids', Integer),
 	Column('id', Integer, primary_key=True),
-	Column('poi_id', Integer),
-	Column('name', String(50)),
-	Column('timestamp', Integer),
-	Column('contact_person', String(50)),
-	Column('contact_number', String(50)),
-	Column('type', String(50)),
-	Column('subtype', String(50)),
-	Column('description', String(200)),
-	Column('status', String(50)),
-	Column('finishtime', String(50))
+	Column('name', Text),
+	Column('time_created', Integer),
+	Column('time_changed', Integer),
+	Column('status', Text),
+	Column('desc', Text),
+	Column('contact_person', Text),
+	Column('contact_number', Text)
 	)
 poi_table =Table('pois', metadata,
-	Column('id', Integer, primary_key=True),
 	Column('coordx', Float),
 	Column('coordy', Float),
-	Column('name', Float),
-	Column('timestamp', Float),
-	Column('type', String(50)),
-	Column('subtype', String(50))
-	)
-alarm_table =Table('alarms', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(50)),
-	Column('timestamp', Float),
-	Column('type', String(50)),
-	Column('subtype', String(50)),
-	Column('contact_number', String(50)),
-	Column('contact_person', String(50)),
-	Column('extra_info', String(200)),
+	Column('name', Text),
+	Column('time_created', Integer),
+	Column('time_changed', Integer),
+	Column('type', Text),
+	Column('subtype', Text)
 	)
+	
+#subtypes:
+#Obstacle:
+
+    #* tree
+    #* bridge
+    #* other
+
+#Structure
+
+    #* hospital
+    #* knullhuset
+    #* base
+    #* pasta_wagon
+    #* other
+
+#Event
+
+    #* fire
+    #* accident
+    #* other	
+	
 unit_table = Table('units',metadata,
-	Column('id', Integer, primary_key=True),
 	Column('coordx', Float),
 	Column('coordy', Float),
-	Column('name', String(50)),
-	Column('timestamp', Float),
-	Column('type', String(50))
+	Column('id', Integer, primary_key=True),
+	Column('name', Text),
+	Column('time_changed', Float),
+	Column('type', Text)
 	)
 		
 message_table = Table('messages',metadata,
 	Column('id', Integer, primary_key=True),
-	Column('sender', String(50)),
-	Column('reciver', String(50)),
-	Column('messagetype', String(50)),
-	Column('timestamp', Integer),
-	Column('content', String(50)),
-	Column('priority', String(50))
+	Column('sender', Text),
+	Column('reciver', Text),
+	Column('type', Text),
+	Column('time_created', Integer),
+	Column('content', Text),
+	Column('response_to', Integer)
 	)
 mission_group = Table('mission_group', metadata,
 	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
@@ -99,14 +111,14 @@ Session.configure(bind=engine)
 
 ##############################definerar classer##################################
 class Message(object):
-	def __init__(self, sender = None, reciver=None, messagetype= None, timestamp=None, content= None, priority= None):
+	def __init__(self, sender=None, reciver=None, type=None, subtype=None, time_created=None, content=None, response_to=None):
 		self.sender=sender
 		self.reciver=reciver
-		self.messagetype=messagetype
-		self.timestamp=timestamp
+		self.type=type
+		self.time_created=time_created
 		self.content=content
-		self.priority=priority
-		
+		self.response_to=response_to
+
 class User(object):
 	def __init__(self, name=None, clearance=None,  password=None):
 		self.name=name
@@ -129,49 +141,36 @@ class Group(object):
 		return self.name
 
 class Mission(object):
-	def __init__(self, poi_id = None, id=None, name= None, timestamp=None, type= None, sub_type= None, description = None, contact_person = None, contact_number = None, status = None, finishtime = None):
+	def __init__(self, poi_id = None, unit_id=None, id=None, name= None, time_created=None, time_changed=None, status = None, desc=None, contact_person = None, contact_number = None):
 		#self.id = generate_id()
 		self.poi_id = poi_id
+		self.unit_id=unit_id
 		self.id=id
 		self.name = name
-		self.timestamp = timestamp
-		self.type = type
+		self.time_created = time_created
+		self.time_changed = time_changed
+		self.status = status
+		self.desc = desc
 		self.contact_person = contact_person
 		self.contact_number = contact_number
-		self.sub_type = sub_type
-		self.description = description
-		self.status = status
-		self.finishtime = finishtime
 
 class Poi(object):
-	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type=None, sub_type= None):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, time_created=None, time_changed=None, type=None, sub_type= None):
 		self.coordx = coordx
 		self.coordy = coordy
 		self.id = id
 		self.name = name
-		self.timestamp = timestamp
+		self.time_created = time_created
 		self.type = type
 		self.sub_type = sub_type
 	
-		
-class Alarm(object):
-	def __init__(self, id=None, name= None, timestamp=None, type= None, poi_id=None, contact_person= None, contact_number= None, extrainfo = None):
-		self.id=id
-		self.name = name
-		self.timestamp = timestamp
-		self.type = type
-		self.poi_id=poi_id
-		self.contact_person = contact_person
-		self.contact_number = contact_number
-		self.extrainfo = extrainfo
-
 class Unit(object):
-	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type= None):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, time_changed=None, type= None):
 		self.coordx = coordx
 		self.coordy = coordy
 		self.id = id
 		self.name = name
-		self.timestamp = timestamp
+		self.time_changed = time_changed
 		self.type = type
 		
 		
@@ -205,7 +204,7 @@ metadata.create_all(engine)
 mapper(Message, message_table)
 mapper(Group, group_table)
 mapper(Poi, poi_table)
-mapper(Alarm, alarm_table)
+
 mapper(Unit, unit_table)
 
 #many to many relationer för att kunna länka grupper till uppdrag
@@ -240,7 +239,9 @@ def get_password(namn):
 
 #retunerar True eller False beroende om en användare finns i databasen eller inte
 def is_user(clientname):
-	user= session.query(User).filter_by(name =clientname).first()
+
+	user= loginSession.query(User).filter_by(name =clientname).first()
+
 	def get_name(user):
 		try:
 			return user.name
@@ -327,11 +328,6 @@ def get_item_all():
 	except:
 		return None
 
-def get_item_all_str():
-	try:
-		return session.query(Item).all()	
-	except:
-		return None
 # Retunerar totala antalet av ett item (summerar)
 def getTotal(namn):	
 	try:	
@@ -360,25 +356,25 @@ def get_mission_by_id(id_nr):
 def get_mission_by_id_all(id_nr):
 	try:
 		m=session.query(Mission).filter_by(id=id_nr).first()
-		return m.name, m.timestamp, m.type, m.description, m.contact_person, m.contact_number, m.status, m.finishtime
+		return m.name, m.time_created, m.time_changed, m.status, m.desc, m.contact_person, m.contact_number
 	except:
 		return None
 #Lägger in ett uppdrag
 #obs! lägg inte in ett uppdrag med samma namn om du vill söka med namn!
-def addMission(name1, timestamp1, type1, description1, contact_person1, contact_number1,status1,finishtime1):
-	session.save(Mission(name=name1, timestamp=timestamp1, type=type1, description=description1, contact_person=contact_person1, contact_number=contact_number1, status=status1, finishtime=finishtime1))	
+def addMission(name1, time_created1, time_changed1, status1, desc1, contact_person1, contact_number1):
+	session.save(Mission(name=name1, time_created=time_created1, time_changed=time_changed1, status=status1, desc=desc1, contact_person=contact_person1, contact_number=contact_number1))	
 	
 #retunerar all uppdragsdata	
 def get_mission_all(namn):
 	try:
 		m= get_mission_object_by_name(namn)
-		return m.name, m.timestamp, m.type, m.description, m.contact_person, m.contact_number, m.status, m.finishtime
+		return m.name, m.time_created, m.time_changed, m.status, m.desc, m.contact_person, m.contact_number
 	except:
 		return None
 
 #lägger in ett medelande i databasen
-def addMessage(sender1, reciver1, messagetype1, timestamp1, content1, priority1):
-	session.save(Message(sender=sender1, reciver=reciver1, messagetype=messagetype1, timestamp=timestamp1, content=content1, priority=priority1))
+def addMessage(sender1, reciver1, type1, time_created1, content1, response_to1):
+	session.save(Message(sender=sender1, reciver=reciver1, type=type1, time_created=time_created1, content=content1, response_to=response_to1))
 #######################################################	
 session = Session()
 USERS = session.query(User).all() # radera kj?
@@ -387,7 +383,7 @@ session.close()
 def getMessage(id_nr):
 	try:
 		m= session.query(Message).filter_by(id=id_nr).first()
-		return m.sender, m.reciver, m.messagetype, m.timestamp, m.content, m.priority
+		return m.sender, m.reciver, m.type, m.time_created, m.content, m.response_to
 	except:
 		return None
 def removeMessage(id_nr):
@@ -406,77 +402,6 @@ def getUnit():
 
 #skapar en session för att kunna komma åt databasen
 session = Session()
-
-#print get_user_groups('maias')
-
-#addMessage('mathias1','hanna','hej',datetime.now(),'hej vad har du haft for dig?', '3')
-#addMessage('mathias2','hanna','hej',datetime.now(),'hej vad har du haft for dig?', '3')
-#addMessage('mathias3','hanna','hej',datetime.now(),'hej vad har du haft for dig?', '3')
-#print getMessage(1)
-#print getMessage(2)
-#print getMessage(3)
-#removeMessage(3)
-#print getMessage(3)
-#print get_user_all()
-##skriver ut valda delar av ett uppdrag
-#m= get_mission_object_by_name('Save the cat')
-#print "get_mission_by_name:"
-#print m.name
-#print m.description
-#print m.id
-#print m.type
-#print is_user('mathias')
-#m = get_mission_by_id(2)
-#print m.name
-#print m.description
-#print m.id
-#print m.type
-## skriver ut allt som finns i ett uppdrag
-#print get_mission_all('Save the cat')
-	
-
-#print "användare som är i: team2 ", get_group_users('team2')
-#print "användare som är i: teamgobject", get_group_users('teamgobject')
-#print "grupper som niklas är med i", get_user_groups('niklas')
-#print "grupper som Mathias är med i", get_user_groups('mathias')
-#print "grupp som inte finns:", get_group('finns inte')
-#print "mathias losen", get_password('mathias')
-#print "alla anvandare", session.query(User).all()
-
-#print 'antalet EMP', getCount('EMP')
-#print "totalt antal EMP",getTotal('EMP')
-
-#print "item Pansarvagn: ",session.query(Item).filter_by(name='Pansarvagn').first()
-#print "en anvandare som inte finns: ", session.query(User).filter_by(name ='m').first()
-
-#print get_group_all()
-
-#print "Skriver ut team2 ", get_group('team2')
-#print "skriver ut anvandare i team2",get_group_users('team2')
-#delete_group_user('Pro','mathias')
-#print "skriver ut anvandare i Pro",get_group_users('Pro')
-#delete_group('team2')
-#print "skriver ut team2 efter borttagning ",get_group('team2')
-#print "skriver ut anvandare i team2",get_group_users('team2')
-#print "skriver ut anvandare i team3",get_group_users('team3')
-#print get_user_groups('mathias')
-#add_group_user('Pro','mathias')
-#print "skriver ut anvandare i Pro",get_group_users('Pro')
-
-#print get_user_groups('mathias')
-#print get_group('Pro')
-#print getTotal('Pansarvagn')
-
-#add_item('Pansarvagn', 10, 'Linkoping')
-#print getTotal('Pansarvagn')
-##print get_mission_by_id(1)
-#addMission("Save the cat",datetime.now(),"kill","Go and kill the cat from the burning tree.", "Daddy Cat", "1987654321","Ongoing","1 hours")
-#print get_mission_all('Save the cat')
-
-#try:
-	#print get_mission_by_id_all(3)
-#except: 
-	#pass
 
 #sparar ändringar i databasen
 session.commit()
