5547d477efd2e312c8a2339340247647eb23d5a7
Mathias Aktan <matak825@sysi-12.sysinst.ida.liu.se>
2009-11-30 11:54:14 +0100

    data

--- kartkomponent/databasklient.py
+++ kartkomponent/databasklient.py
@@ -9,24 +9,32 @@ from datetime import *
 #skapar databasen
 engine = create_engine('sqlite:///dataClient.db', echo=False)
 metadata = MetaData()
- 
+
+id_nr=0
+def generate_id():
+	global id_nr
+	id_nr+=1
+	id=(id_nr*10)+2
+	return id
+
+#######################skapar data tabeller############################33
 group_table = Table('group', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(20))
+	Column('name', Text)
 	)
 	
 user_table = Table('users', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(20)),
-	Column('clearance', String(40)),
-	Column('password', String(40))
+	Column('name', Text),
+	Column('clearance', Text),
+	Column('password', Text)
 	)
 	
 items_table = Table('items_table', metadata,
 	Column('item_id', Integer, primary_key=True),
-	Column('name', String(40)),
+	Column('name', Text),
 	Column('count', Integer),
-	Column('location', String(40))
+	Column('location', Text)
 	)
 
 user_group = Table('user_group', metadata,
@@ -35,172 +43,193 @@ user_group = Table('user_group', metadata,
 	)
 	
 mission_table =Table('missions', metadata,
+	Column('poi_ids', Integer),
+	Column('unit_ids', Integer),
 	Column('id', Integer, primary_key=True),
-	Column('poi_id', Integer),
-	Column('name', String(50)),
-	Column('timestamp', Integer),
-	Column('contact_person', String(50)),
-	Column('contact_number', String(50)),
-	Column('type', String(50)),
-	Column('subtype', String(50)),
-	Column('description', String(200)),
-	Column('status', String(50)),
-	Column('finishtime', String(50))
+	Column('name', Text),
+	Column('time_created', Integer),
+	Column('time_changed', Integer),
+	Column('status', Text),
+	Column('desc', Text),
+	Column('contact_person', Text),
+	Column('contact_number', Text)
 	)
 poi_table =Table('pois', metadata,
-	Column('id', Integer, primary_key=True),
 	Column('coordx', Float),
 	Column('coordy', Float),
-	Column('name', Float),
-	Column('timestamp', Float),
-	Column('type', String(50)),
-	Column('subtype', String(50))
-	)
-alarm_table =Table('alarms', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('name', String(50)),
-	Column('timestamp', Float),
-	Column('type', String(50)),
-	Column('subtype', String(50)),
-	Column('contact_number', String(50)),
-	Column('contact_person', String(50)),
-	Column('extra_info', String(200)),
+	Column('name', Text),
+	Column('time_created', Integer),
+	Column('time_changed', Integer),
+	Column('type', Text),
+	Column('subtype', Text)
 	)
+
 unit_table = Table('units',metadata,
-	Column('id', Integer, primary_key=True),
 	Column('coordx', Float),
 	Column('coordy', Float),
-	Column('name', String(50)),
-	Column('timestamp', Float),
-	Column('type', String(50))
+	Column('id', Integer, primary_key=True),
+	Column('name', Text),
+	Column('time_changed', Integer),
+	Column('type', Text)
 	)
 		
 message_table = Table('messages',metadata,
 	Column('id', Integer, primary_key=True),
-	Column('sender', String(50)),
-	Column('reciver', String(50)),
-	Column('messagetype', String(50)),
-	Column('timestamp', Integer),
-	Column('content', String(50)),
-	Column('priority', String(50))
+	Column('sender', Text),
+	Column('receiver', Text),
+	Column('type', Text),
+	Column('time_created', Integer),
+	Column('content', Text),
+	Column('response_to', Integer)
 	)
 mission_group = Table('mission_group', metadata,
 	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
 	Column('group_id', None, ForeignKey('group.id'), primary_key=True)
 	)
-	
-		
+mission_poi = Table ('mission_poi', metadata,
+	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
+	Column('poi_id', None, ForeignKey('pois.id'), primary_key=True)
+	)
+mission_unit = Table ('mission_unit', metadata,
+	Column('mission_id', None, ForeignKey('missions.id'), primary_key=True),
+	Column('unit_id', None, ForeignKey('units.id'), primary_key=True)
+	)
+
+#############################################################################
+
 #definerar Session och kopplar den till databasen
 Session = sessionmaker()
 Session.configure(bind=engine)
 
 ##############################definerar classer##################################
 class Message(object):
-	def __init__(self, sender = None, reciver=None, messagetype= None, timestamp=None, content= None, priority= None):
+	def __init__(self, id=None, sender=None, receiver=None, type=None, time_created=None, content=None, response_to=None):
+		self.id=generate_id()
 		self.sender=sender
-		self.reciver=reciver
-		self.messagetype=messagetype
-		self.timestamp=timestamp
+		self.receiver=receiver
+		self.type=type
+		self.time_created=time_created
 		self.content=content
-		self.priority=priority
-		
+		self.response_to=response_to
+
 class User(object):
-	def __init__(self, name=None, clearance=None,  password=None):
+	def __init__(self, name=None, id=None, clearance=None,  password=None):
 		self.name=name
+		self.id=generate_id()
 		self.clearance=clearance
 		self.password=password	
 	def __repr__(self):
 		return self.name
 class Item(object):
-	def __init__(self, name=None, count=None, location=None):
+	def __init__(self, name=None, id=None, count=None, location=None):
 		self.name=name
+		self.id=generate_id()
 		self.count=count
 		self.location=location
 	def __repr__(self):
 		return self.name, self.count, self.location
 class Group(object):
 	
-	def __init__(self, name=None):
+	def __init__(self, name=None, id=None):
 		self.name=name
-	def __repr__(self):
-		return self.name
+		self.id=generate_id()
+	
 
 class Mission(object):
-	def __init__(self, poi_id = None, id=None, name= None, timestamp=None, type= None, sub_type= None, description = None, contact_person = None, contact_number = None, status = None, finishtime = None):
+	def __init__(self, poi_id = None, unit_id=None, id=None, name= None, time_created=None, time_changed=None, status = None, desc=None, contact_person = None, contact_number = None):
 		#self.id = generate_id()
 		self.poi_id = poi_id
-		self.id=id
+		self.unit_id=unit_id
+		self.id=generate_id()
 		self.name = name
-		self.timestamp = timestamp
-		self.type = type
+		self.time_created = time_created
+		self.time_changed = time_changed
+		self.status = status
+		self.desc = desc
 		self.contact_person = contact_person
 		self.contact_number = contact_number
-		self.sub_type = sub_type
-		self.description = description
-		self.status = status
-		self.finishtime = finishtime
 
 class Poi(object):
-	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type=None, sub_type= None):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, time_created=None, time_changed=None, type=None, sub_type= None):
 		self.coordx = coordx
 		self.coordy = coordy
-		self.id = id
+		self.id=generate_id()
 		self.name = name
-		self.timestamp = timestamp
+		self.time_created = time_created
 		self.type = type
 		self.sub_type = sub_type
 	
-		
-class Alarm(object):
-	def __init__(self, id=None, name= None, timestamp=None, type= None, poi_id=None, contact_person= None, contact_number= None, extrainfo = None):
-		self.id=id
-		self.name = name
-		self.timestamp = timestamp
-		self.type = type
-		self.poi_id=poi_id
-		self.contact_person = contact_person
-		self.contact_number = contact_number
-		self.extrainfo = extrainfo
-
 class Unit(object):
-	def __init__(self, coordx= None, coordy= None, id=None, name= None, timestamp=None, type= None):
+	def __init__(self, coordx= None, coordy= None, id=None, name= None, time_changed=None, type= None):
 		self.coordx = coordx
 		self.coordy = coordy
-		self.id = id
+		self.id=generate_id()
 		self.name = name
-		self.timestamp = timestamp
+		self.time_changed = time_changed
 		self.type = type
-				
 		
+		
+def dbClass(cl):
+	if(cl == "message"):
+		return Message()
+
+	if(cl == "user"):
+		return User()
+
+	if(cl == "item"):
+		return Item()
+
+	if(cl == "group"):
+		return Group()
+
+	if(cl == "mission"):
+		return Mission()
+
+	if(cl == "poi"):
+		return Poi()
+
+	if(cl == "unit"):
+		return Unit()
+
+	return None
+
 metadata.create_all(engine)
 
 #länkar classobject till datatabeller
-mapper(Message, message_table)
-mapper(Group, group_table)
-mapper(Poi, poi_table)
-mapper(Alarm, alarm_table)
-mapper(Unit, unit_table)
+mapper(Message, message_table, properties=dict())
+mapper(Group, group_table, properties=dict())
+mapper(Poi, poi_table, properties=dict())
+mapper(Unit, unit_table, properties=dict())
 
 #many to many relationer för att kunna länka grupper till uppdrag
-mapper(Mission, mission_table, properties=dict(
-	groups=relation(Group, secondary= mission_group, backref='missions'))
+mapper(Mission, mission_table, properties={
+	'groups': relation(Group, secondary= mission_group, backref='missions'),
+	'pois': relation(Poi, secondary= mission_poi, backref='missions'),
+	'units': relation(Unit, secondary= mission_unit, backref='missions')
+	}
 	)
 	
 #many to many relation för att kunna länka användarkonton till grupper
-mapper(User, user_table, properties=dict(
-	groups=relation(Group, secondary= user_group, backref='users'))
+mapper(User, user_table, properties={
+	'groups': relation(Group, secondary= user_group, backref='users')}
 	)
 
-mapper(Item, items_table)
+mapper(Item, items_table, properties=dict())
 
 
 
 ############################Metoder###########################
 
+
+
+
 #retunerar alla användare
 def get_user_all():
-	return session.query(User).all()
-
+	try:
+		return session.query(User).all()
+	except:
+		return None
 #retunerar lösenord hos en användare
 def get_password(namn):
 	try:
@@ -211,7 +240,9 @@ def get_password(namn):
 
 #retunerar True eller False beroende om en användare finns i databasen eller inte
 def is_user(clientname):
-	user= session.query(User).filter_by(name =clientname).first()
+
+	user= loginSession.query(User).filter_by(name =clientname).first()
+
 	def get_name(user):
 		try:
 			return user.name
@@ -270,8 +301,10 @@ def add_group_user(groupname,username):
 
 #retunerar alla grupper
 def get_group_all():
-	return session.query(Group).all()
-
+	try:	
+		return session.query(Group).all()
+	except:
+		return None
 #retunerar namnet på en grupp 
 def get_group(namn):
 	try:
@@ -291,8 +324,11 @@ def add_item(name1,count1,location1):
 	session.save(Item(name=name1,count=count1,location=location1))
 	
 def get_item_all():
-	return session.query(Item).all()
-	
+	try:
+		return session.query(Item).all()	
+	except:
+		return None
+
 # Retunerar totala antalet av ett item (summerar)
 def getTotal(namn):	
 	try:	
@@ -321,45 +357,107 @@ def get_mission_by_id(id_nr):
 def get_mission_by_id_all(id_nr):
 	try:
 		m=session.query(Mission).filter_by(id=id_nr).first()
-		return m.name, m.timestamp, m.type, m.description, m.contact_person, m.contact_number, m.status, m.finishtime
+		return m.name, m.time_created, m.time_changed, m.status, m.desc, m.contact_person, m.contact_number
 	except:
 		return None
 #Lägger in ett uppdrag
 #obs! lägg inte in ett uppdrag med samma namn om du vill söka med namn!
-def addMission(name1, timestamp1, type1, description1, contact_person1, contact_number1,status1,finishtime1):
-	session.save(Mission(name=name1, timestamp=timestamp1, type=type1, description=description1, contact_person=contact_person1, contact_number=contact_number1, status=status1, finishtime=finishtime1))	
+def addMission(name1, time_created1, time_changed1, status1, desc1, contact_person1, contact_number1):
+	session.save(Mission(name=name1, time_created=time_created1, time_changed=time_changed1, status=status1, desc=desc1, contact_person=contact_person1, contact_number=contact_number1))	
 	
 #retunerar all uppdragsdata	
 def get_mission_all(namn):
 	try:
 		m= get_mission_object_by_name(namn)
-		return m.name, m.timestamp, m.type, m.description, m.contact_person, m.contact_number, m.status, m.finishtime
+		return m.name, m.time_created, m.time_changed, m.status, m.desc, m.contact_person, m.contact_number
 	except:
 		return None
 
+def getPoi(namn):
+	try:
+		m=session.query(Poi).filter_by(name=namn).first()
+		return m
+	except:
+		return None
+def addPoi(coordx1, coordy1,name1,time_created1,type1,sub_type1):
+	session.save(Poi(coordx=coordx1, coordy=coordy1, name=name1, time_created=time_created1, type=type1, sub_type=sub_type1))
+#lägger in användare i en grupp	
+def add_mission_poi(mission_id,poi_id):
+	try:
+		m=session.query(Mission).filter_by(id=mission_id).first()
+		p=session.query(Poi).filter_by(id=poi_id).first()
+		m.pois.append(p)
+		
+	except:
+		pass
+	
 #lägger in ett medelande i databasen
-def addMessage(sender1, reciver1, messagetype1, timestamp1, content1, priority1):
-	session.save(Message(sender=sender1, reciver=reciver1, messagetype=messagetype1, timestamp=timestamp1, content=content1, priority=priority1))
+def addMessage(sender1, receiver1, type1, time_created1, content1, response_to1):
+	session.save(Message(sender=sender1, receiver=receiver1, type=type1, time_created=time_created1, content=content1, response_to=response_to1))
+	
+	
+	
+	
 #######################################################	
 session = Session()
 USERS = session.query(User).all() # radera kj?
 session.close()
 
 def getMessage(id_nr):
-	try:
-		m= session.query(Message).filter_by(id=id_nr).first()
-		return m.sender, m.reciver, m.messagetype, m.timestamp, m.content, m.priority
-	except:
-		return None
+	#try:
+		m=session.query(Message).filter_by(id=id_nr).first()
+		return m
+	#except:
+	#	return None
 def removeMessage(id_nr):
 	m=session.query(Message).filter_by(id=id_nr).first()
 	session.delete(m)
-###########################Spårutskrifter############################	
+	
+def addUnit(coordx1, coordy1, name1, time_changed1, type1):
+	session.save(Unit(coordx=coordx1, coordy=coordy1, name=name1, time_changed=time_changed1, type=type1))
 
-#skapar en session för att kunna komma åt databasen
-session = Session()
-#print get_item_all()
-session.commit()
+def add_mission_unit(mission_id, unit_id):
+	try:
+		m=session.query(Mission).filter_by(id=mission_id).first()
+		u=session.query(Unit).filter_by(id=unit_id).first()
+		m.units.append(u)
+		
+	except:
+		pass
+
+def removeUnit():
+	pass
+
+
+
+def getUnits():
+	try:
+		m=session.query(Unit).first()
+		return m.coordx,m.coordy,m.name,m.time_changed,m.type
+	except:
+		return None
+def getUnit(namn):
+	m=session.query(Unit).filter_by(name=namn).first()
+	return m
+	
+def class2dict(o):
+    """Return a dictionary from object that has public
+       variable -> key pairs
+    """    
+    dict = {}
+    #Joy: all the attributes in a class are already in __dict__
+    for elem in o.__dict__.keys():
+        if elem.find("_" + o.__class__.__name__) == 0:
+            continue
+            #We discard private variables, which are automatically
+            #named _ClassName__variablename, when we define it in
+            #the class as __variablename
+        else:
+            dict[elem] = o.__dict__[elem]
+	    if(str(dict[elem]).startswith('<') and not str(elem.startswith('_'))):#bugg: man far inte borja ett meddelande med <
+		   dict[elem] = class2dict(o.__dict__[elem])
+    return dict	
+
 
 
 
